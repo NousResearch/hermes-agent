0e09dcc15a7b03dd77bf48dd094802cb1a8d4712
adybag14-cyber <adybag14@gmail.com>
2026-05-04 08:35:08 +0100

    feat(android): simplify local model setup

--- tools/delegate_tool.py
+++ tools/delegate_tool.py
@@ -39,7 +39,7 @@ from toolsets import TOOLSETS
 _RUNTIME_PROVIDER_CUSTOM = "custom"
 from tools import file_state
 from tools.terminal_tool import set_approval_callback as _set_subagent_approval_cb
-from utils import base_url_hostname, is_truthy_value
+from hermes_cli.shared_utils import base_url_hostname, is_truthy_value
 
 
 # Tools that children must never have access to

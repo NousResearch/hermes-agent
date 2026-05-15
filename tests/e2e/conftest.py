ab325aa91ec00a9e8531071700845e092d216213
Lubrsy706 <lubrsy706@gmail.com>
2026-05-15 12:07:00 +0800

    test(e2e): make discord forbidden mock catchable

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -67,6 +67,7 @@ def _ensure_discord_mock():
     discord_mod.Thread = type("Thread", (), {})
     discord_mod.ForumChannel = type("ForumChannel", (), {})
     discord_mod.Interaction = object
+    discord_mod.Forbidden = type("Forbidden", (Exception,), {})
     discord_mod.app_commands = SimpleNamespace(
         describe=lambda **kwargs: (lambda fn: fn),
         choices=lambda **kwargs: (lambda fn: fn),

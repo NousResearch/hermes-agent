4d032ca8ac62be2a65f2ae95c3c381d30fe30571
Veritas-7 <veritas-7@users.noreply.github.com>
2026-06-15 23:48:36 +0900

    chore(release): map Veritas noreply author

--- scripts/release.py
+++ scripts/release.py
@@ -45,6 +45,7 @@ ACP_REGISTRY_MANIFEST = REPO_ROOT / "acp_registry" / "agent.json"
 
 # Auto-extracted from noreply emails + manual overrides
 AUTHOR_MAP = {
+    "veritas-7@users.noreply.github.com": "Veritas-7",
     "kenmege@yahoo.com": "Kenmege",
     "tianying.x@eukarya.io": "xtymac",
     "dkobi16@gmail.com": "Diyoncrz18",

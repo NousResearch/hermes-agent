AnandaAnugrahHandyanto/savarez_agent

fix: migrate goals at compression boundary

agent/conversation_compression.py
@@ -547,6 +547,12 @@ def compress_context(
                 except (ValueError, Exception) as e:
                     logger.debug("Could not propagate title on compression: %s", e)
             agent._session_db.update_system_prompt(agent.session_id, new_system_prompt)
+            try:
+                from hermes_cli.goals import migrate_goal_session
+
+                migrate_goal_session(old_session_id, agent.session_id, db=agent._session_db)
+            except Exception as goal_err:
+                logger.debug("GoalManager migration on compression failed: %s", goal_err)
             # Reset flush cursor — new session starts with no messages written
             agent._last_flushed_db_idx = 0
         except Exception as e:

NousResearch/hermes-agent

agent/conversation_compression.py
@@ -644,10 +644,10 @@ def compress_context(
                 # per-session lookup with no parent walk, so without this an
                 # active goal silently dies at the boundary (#33618).
                 try:
-                    from hermes_cli.goals import migrate_goal_to_session
-                    migrate_goal_to_session(old_session_id, agent.session_id, reason="compression")
+                    from hermes_cli.goals import migrate_goal_session
+                    migrate_goal_session(old_session_id, agent.session_id, db=agent._session_db)
                 except Exception as _goal_err:
-                    logger.debug("Could not migrate goal on compression: %s", _goal_err)
+                    logger.debug("GoalManager migration on compression failed: %s", _goal_err)
                 # Auto-number the title for the continuation session
                 if old_title:
                     try:
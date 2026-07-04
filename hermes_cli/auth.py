ai-integr8tor/NousResearch-hermes-agent

feat(cli): add auto-discovery registry mappings for Mistral, Cohere, DeepInfra, and SiliconFlow (#58029)

hermes_cli/auth.py
@@ -192,6 +192,38 @@ PROVIDER_REGISTRY: Dict[str, ProviderConfig] = {
         api_key_env_vars=("OPENAI_API_KEY",),
         base_url_env_var="OPENAI_BASE_URL",
     ),
+    "mistral": ProviderConfig(
+        id="mistral",
+        name="Mistral AI",
+        auth_type="api_key",
+        inference_base_url="https://api.mistral.ai/v1",
+        api_key_env_vars=("MISTRAL_API_KEY",),
+        base_url_env_var="MISTRAL_BASE_URL",
+    ),
+    "cohere": ProviderConfig(
+        id="cohere",
+        name="Cohere",
+        auth_type="api_key",
+        inference_base_url="https://api.cohere.com/v1",
+        api_key_env_vars=("COHERE_API_KEY", "COHERE_KEY"),
+        base_url_env_var="COHERE_BASE_URL",
+    ),
+    "deepinfra": ProviderConfig(
+        id="deepinfra",
+        name="DeepInfra",
+        auth_type="api_key",
+        inference_base_url="https://api.deepinfra.com/v1/openai",
+        api_key_env_vars=("DEEPINFRA_API_KEY",),
+        base_url_env_var="DEEPINFRA_BASE_URL",
+    ),
+    "siliconflow": ProviderConfig(
+        id="siliconflow",
+        name="SiliconFlow",
+        auth_type="api_key",
+        inference_base_url="https://api.siliconflow.cn/v1",
+        api_key_env_vars=("SILICONFLOW_API_KEY",),
+        base_url_env_var="SILICONFLOW_BASE_URL",
+    ),
     "xai-oauth": ProviderConfig(
         id="xai-oauth",
         name="xAI Grok OAuth (SuperGrok / Premium+)",

NousResearch/hermes-agent

hermes_cli/auth.py
@@ -192,6 +192,38 @@ PROVIDER_REGISTRY: Dict[str, ProviderConfig] = {
         api_key_env_vars=("OPENAI_API_KEY",),
         base_url_env_var="OPENAI_BASE_URL",
     ),
+    "mistral": ProviderConfig(
+        id="mistral",
+        name="Mistral AI",
+        auth_type="api_key",
+        inference_base_url="https://api.mistral.ai/v1",
+        api_key_env_vars=("MISTRAL_API_KEY",),
+        base_url_env_var="MISTRAL_BASE_URL",
+    ),
+    "cohere": ProviderConfig(
+        id="cohere",
+        name="Cohere",
+        auth_type="api_key",
+        inference_base_url="https://api.cohere.ai/v1",
+        api_key_env_vars=("COHERE_API_KEY", "COHERE_KEY"),
+        base_url_env_var="COHERE_BASE_URL",
+    ),
+    "deepinfra": ProviderConfig(
+        id="deepinfra",
+        name="DeepInfra",
+        auth_type="api_key",
+        inference_base_url="https://api.deepinfra.com/v1/models",
+        api_key_env_vars=("DEEPINFRA_API_KEY",),
+        base_url_env_var="DEEPINFRA_BASE_URL",
+    ),
+    "siliconflow": ProviderConfig(
+        id="siliconflow",
+        name="SiliconFlow",
+        auth_type="api_key",
+        inference_base_url="https://api.siliconflow.com/v1/models",
+        api_key_env_vars=("SILICONFLOW_API_KEY",),
+        base_url_env_var="SILICONFLOW_BASE_URL",
+    ),
     "xai-oauth": ProviderConfig(
         id="xai-oauth",
         name="xAI Grok OAuth (SuperGrok / Premium+)",
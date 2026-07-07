burgebj/hermes

fix(kanban): confirm corrupt boards before backoff (#33486)

tests/hermes_cli/test_kanban_core_functionality.py
@@ -3617,7 +3617,7 @@ def test_gateway_dispatcher_watcher_env_truthy_uses_config(monkeypatch):
 def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
     monkeypatch, tmp_path, caplog, corrupt_exc
 ):
-    """Corrupt board DBs log one actionable error and stop retrying per tick."""
+    """Corrupt board DBs log one actionable error and back off per tick."""
     import asyncio
     import logging
     import sqlite3
@@ -3638,6 +3638,7 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
             "kanban": {
                 "dispatch_in_gateway": True,
                 "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
             }
         },
     )
@@ -3666,13 +3667,6 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
         raise sqlite3.DatabaseError("file is not a database")
 
     async def _to_thread(fn, *args, **kwargs):
-        # PR salvage (#32857 commit 7): the dispatcher now reaps zombies at
-        # the top of each tick via ``asyncio.to_thread(_kb.reap_worker_zombies)``
-        # BEFORE the per-board tick work. Each tick now issues 3 ``to_thread``
-        # calls (reaper + ``_tick_once`` + ``_ready_nonempty``) instead of 2,
-        # so this counter must reach 6 to allow the same 2 dispatch ticks the
-        # pre-reaper test expected at 4. Connect counts in the assertion below
-        # are unchanged.
         calls["to_thread"] += 1
         result = fn(*args, **kwargs)
         if calls["to_thread"] >= 6:
@@ -3698,19 +3692,16 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
     assert sum("not a valid SQLite database" in msg for msg in messages) == 1
     assert not any("tick failed on board" in msg for msg in messages)
     assert not any(record.exc_info for record in caplog.records)
-    # First tick connect (dispatch) + two probes per `_has_ready_work` call
-    # (ready then review, both via _kb.connect). The second dispatch tick
-    # skips the dispatch connect because the corrupt board fingerprint is
-    # disabled, but the ready/review probes still each connect. PR f55d94a1e
-    # added the review-column probe alongside the existing ready-column
-    # probe, bumping this from 3 → 5.
-    assert calls["connect"] == 5
+    # First tick connect (dispatch) + one swallowed health-probe connect
+    # per tick. The second dispatch tick skips the dispatch connect because
+    # the corrupt board fingerprint is disabled.
+    assert calls["connect"] == 3
 
 
-def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
+def test_gateway_dispatcher_retries_corrupt_board_after_backoff(
     monkeypatch, tmp_path, caplog
 ):
-    """A corrupt-looking board is retried after the quarantine TTL expires."""
+    """A confirmed corrupt board is retried after the backoff expires."""
     import asyncio
     import inspect
     import logging
@@ -3732,6 +3723,7 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
             "kanban": {
                 "dispatch_in_gateway": True,
                 "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
             }
         },
     )
@@ -3748,13 +3740,19 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
     monkeypatch.setattr(_kb, "kanban_db_path", lambda board=None: corrupt_db)
 
     real_monotonic = time.monotonic
-    time_values = iter([1000.0, 1001.0, 1301.0, 1301.0])
+    time_values = iter([1000.0, 1030.0, 1030.0])
 
     def _monotonic_for_gateway_dispatcher():
         caller = inspect.currentframe().f_back  # type: ignore[union-attr]
         code = caller.f_code if caller is not None else None
         filename = code.co_filename if code is not None else ""
-        if filename.endswith("gateway/run.py"):
+        function_name = code.co_name if code is not None else ""
+        if filename.replace("\\", "/").endswith(
+            "gateway/run.py"
+        ) and function_name in {
+            "_record_confirmed_corrupt_board",
+            "_tick_once_for_board",
+        }:
             return next(time_values, 1301.0)
         return real_monotonic()
 
@@ -3766,10 +3764,15 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
         raise sqlite3.DatabaseError("file is not a database")
 
     async def _to_thread(fn, *args, **kwargs):
+        name = getattr(fn, "__name__", "")
+        if name == "reap_worker_zombies":
+            return []
+        if name == "_ready_nonempty":
+            return False
         result = fn(*args, **kwargs)
-        if getattr(fn, "__name__", "") == "_tick_once":
+        if name == "_tick_once":
             calls["tick"] += 1
-            if calls["tick"] >= 3:
+            if calls["tick"] >= 2:
                 runner._running = False
         return result
 
@@ -3790,8 +3793,8 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
 
     messages = [record.getMessage() for record in caplog.records]
     assert sum("not a valid SQLite database" in msg for msg in messages) == 2
-    assert any("database fingerprint unchanged" in msg for msg in messages)
-    assert calls["tick"] == 3
+    assert any("after 30s corrupt-board backoff" in msg for msg in messages)
+    assert calls["tick"] == 2
 
 
 # ---------------------------------------------------------------------------

tests/hermes_cli/test_kanban_dispatcher_resilience.py
@@ -1,0 +1,222 @@
+from __future__ import annotations
+
+import asyncio
+import inspect
+import logging
+import sqlite3
+import time
+from types import SimpleNamespace
+
+
+class _DummyConn:
+    def close(self) -> None:
+        pass
+
+
+def _install_gateway_dispatcher_harness(
+    monkeypatch,
+    tmp_path,
+    *,
+    dispatch_once,
+    stop_after_ticks: int,
+    monotonic_values: list[float] | None = None,
+    after_tick=None,
+):
+    from gateway.run import GatewayRunner
+    import hermes_cli.config as _cfg_mod
+    import hermes_cli.kanban_db as _kb
+
+    db_path = tmp_path / "kanban.db"
+    runner = object.__new__(GatewayRunner)
+    runner._running = True
+    calls = {"ticks": 0}
+
+    monkeypatch.setattr(
+        _cfg_mod,
+        "load_config",
+        lambda: {
+            "kanban": {
+                "dispatch_in_gateway": True,
+                "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
+            }
+        },
+    )
+    monkeypatch.setattr(
+        _kb,
+        "list_boards",
+        lambda include_archived=False: [{"slug": _kb.DEFAULT_BOARD}],
+    )
+    monkeypatch.setattr(_kb, "read_board_metadata", lambda slug: {"slug": slug})
+    monkeypatch.setattr(_kb, "kanban_db_path", lambda board=None: db_path)
+    monkeypatch.setattr(_kb, "connect", lambda *args, **kwargs: _DummyConn())
+    monkeypatch.setattr(_kb, "dispatch_once", dispatch_once)
+
+    async def _to_thread(fn, *args, **kwargs):
+        name = getattr(fn, "__name__", "")
+        if name == "reap_worker_zombies":
+            return []
+        if name == "_ready_nonempty":
+            return False
+        result = fn(*args, **kwargs)
+        if name == "_tick_once":
+            calls["ticks"] += 1
+            if after_tick is not None:
+                after_tick(calls["ticks"])
+            if calls["ticks"] >= stop_after_ticks:
+                runner._running = False
+        return result
+
+    async def _sleep(_delay):
+        return None
+
+    monkeypatch.setattr("gateway.run.asyncio.to_thread", _to_thread)
+    monkeypatch.setattr("gateway.run.asyncio.sleep", _sleep)
+
+    if monotonic_values is not None:
+        values = iter(monotonic_values)
+        fallback = monotonic_values[-1]
+        real_monotonic = time.monotonic
+
+        def _monotonic_for_gateway_dispatcher():
+            caller = inspect.currentframe().f_back  # type: ignore[union-attr]
+            code = caller.f_code if caller is not None else None
+            filename = code.co_filename if code is not None else ""
+            function_name = code.co_name if code is not None else ""
+            if filename.replace("\\", "/").endswith(
+                "gateway/run.py"
+            ) and function_name in {
+                "_record_confirmed_corrupt_board",
+                "_tick_once_for_board",
+            }:
+                return next(values, fallback)
+            return real_monotonic()
+
+        monkeypatch.setattr("gateway.run.time.monotonic", _monotonic_for_gateway_dispatcher)
+
+    return runner, db_path, calls
+
+
+def _run_watcher(runner) -> None:
+    asyncio.run(asyncio.wait_for(runner._kanban_dispatcher_watcher(), timeout=3.0))
+
+
+def test_gateway_dispatcher_quick_check_ok_does_not_latch(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=2,
+    )
+    with sqlite3.connect(db_path) as conn:
+        conn.execute("CREATE TABLE marker(id INTEGER)")
+
+    with caplog.at_level(logging.WARNING, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 2
+    assert dispatch_calls["count"] == 2
+    assert not any("not a valid SQLite database" in msg for msg in messages)
+    assert sum("read-only quick_check passed" in msg for msg in messages) == 2
+
+
+def test_gateway_dispatcher_confirmed_corruption_doubles_backoff(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=3,
+        monotonic_values=[1000.0, 1029.0, 1030.0, 1030.0],
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 3
+    assert dispatch_calls["count"] == 2
+    assert any("for 30s" in msg for msg in messages)
+    assert any("for 60s" in msg for msg in messages)
+    assert any("after 30s corrupt-board backoff" in msg for msg in messages)
+
+
+def test_gateway_dispatcher_fingerprint_change_retries_and_resets_backoff(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    def _after_tick(tick: int) -> None:
+        if tick == 1:
+            db_path.write_text("not sqlite but changed", encoding="utf-8")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=2,
+        monotonic_values=[1000.0, 1001.0, 1001.0],
+        after_tick=_after_tick,
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 2
+    assert dispatch_calls["count"] == 2
+    assert sum("for 30s" in msg for msg in messages) == 2
+    assert not any("for 60s" in msg for msg in messages)
+    assert any("database changed; retrying dispatch" in msg for msg in messages)
+
+
+def test_gateway_dispatcher_success_clears_disabled_board_state(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        if dispatch_calls["count"] in {1, 3}:
+            raise sqlite3.DatabaseError("database disk image is malformed")
+        return SimpleNamespace(spawned=[])
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=3,
+        monotonic_values=[1000.0, 1030.0, 1031.0],
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 3
+    assert dispatch_calls["count"] == 3
+    assert sum("for 30s" in msg for msg in messages) == 2
+    assert not any("for 60s" in msg for msg in messages)

NousResearch/hermes-agent

tests/hermes_cli/test_kanban_core_functionality.py
@@ -3673,7 +3673,7 @@ def test_gateway_dispatcher_watcher_env_truthy_uses_config(monkeypatch):
 def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
     monkeypatch, tmp_path, caplog, corrupt_exc
 ):
-    """Corrupt board DBs log one actionable error and stop retrying per tick."""
+    """Corrupt board DBs log one actionable error and back off per tick."""
     import asyncio
     import logging
     import sqlite3
@@ -3694,6 +3694,7 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
             "kanban": {
                 "dispatch_in_gateway": True,
                 "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
             }
         },
     )
@@ -3722,13 +3723,6 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
         raise sqlite3.DatabaseError("file is not a database")
 
     async def _to_thread(fn, *args, **kwargs):
-        # PR salvage (#32857 commit 7): the dispatcher now reaps zombies at
-        # the top of each tick via ``asyncio.to_thread(_kb.reap_worker_zombies)``
-        # BEFORE the per-board tick work. Each tick now issues 3 ``to_thread``
-        # calls (reaper + ``_tick_once`` + ``_ready_nonempty``) instead of 2,
-        # so this counter must reach 6 to allow the same 2 dispatch ticks the
-        # pre-reaper test expected at 4. Connect counts in the assertion below
-        # are unchanged.
         calls["to_thread"] += 1
         result = fn(*args, **kwargs)
         if calls["to_thread"] >= 6:
@@ -3754,19 +3748,16 @@ def test_gateway_dispatcher_disables_corrupt_board_without_traceback(
     assert sum("not a valid SQLite database" in msg for msg in messages) == 1
     assert not any("tick failed on board" in msg for msg in messages)
     assert not any(record.exc_info for record in caplog.records)
-    # First tick connect (dispatch) + two probes per `_has_ready_work` call
-    # (ready then review, both via _kb.connect). The second dispatch tick
-    # skips the dispatch connect because the corrupt board fingerprint is
-    # disabled, but the ready/review probes still each connect. PR f55d94a1e
-    # added the review-column probe alongside the existing ready-column
-    # probe, bumping this from 3 → 5.
-    assert calls["connect"] == 5
+    # First tick connect (dispatch) + one swallowed health-probe connect
+    # per tick. The second dispatch tick skips the dispatch connect because
+    # the corrupt board fingerprint is disabled.
+    assert calls["connect"] == 3
 
 
-def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
+def test_gateway_dispatcher_retries_corrupt_board_after_backoff(
     monkeypatch, tmp_path, caplog
 ):
-    """A corrupt-looking board is retried after the quarantine TTL expires."""
+    """A confirmed corrupt board is retried after the backoff expires."""
     import asyncio
     import inspect
     import logging
@@ -3788,6 +3779,7 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
             "kanban": {
                 "dispatch_in_gateway": True,
                 "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
             }
         },
     )
@@ -3804,16 +3796,19 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
     monkeypatch.setattr(_kb, "kanban_db_path", lambda board=None: corrupt_db)
 
     real_monotonic = time.monotonic
-    time_values = iter([1000.0, 1001.0, 1301.0, 1301.0])
+    time_values = iter([1000.0, 1030.0, 1030.0])
 
     def _monotonic_for_gateway_dispatcher():
         caller = inspect.currentframe().f_back  # type: ignore[union-attr]
         code = caller.f_code if caller is not None else None
         filename = code.co_filename if code is not None else ""
-        # The kanban dispatcher/notifier watcher loops were extracted from
-        # gateway/run.py into gateway/kanban_watchers.py (god-file Phase 3),
-        # so accept either filename for the time-travel mock.
-        if filename.endswith("gateway/run.py") or filename.endswith("gateway/kanban_watchers.py"):
+        function_name = code.co_name if code is not None else ""
+        if filename.replace("\\", "/").endswith(
+            "gateway/kanban_watchers.py"
+        ) and function_name in {
+            "_record_confirmed_corrupt_board",
+            "_tick_once_for_board",
+        }:
             return next(time_values, 1301.0)
         return real_monotonic()
 
@@ -3826,10 +3821,15 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
         raise sqlite3.DatabaseError("file is not a database")
 
     async def _to_thread(fn, *args, **kwargs):
+        name = getattr(fn, "__name__", "")
+        if name == "reap_worker_zombies":
+            return []
+        if name == "_ready_nonempty":
+            return False
         result = fn(*args, **kwargs)
-        if getattr(fn, "__name__", "") == "_tick_once":
+        if name == "_tick_once":
             calls["tick"] += 1
-            if calls["tick"] >= 3:
+            if calls["tick"] >= 2:
                 runner._running = False
         return result
 
@@ -3850,8 +3850,8 @@ def test_gateway_dispatcher_retries_corrupt_board_after_quarantine(
 
     messages = [record.getMessage() for record in caplog.records]
     assert sum("not a valid SQLite database" in msg for msg in messages) == 2
-    assert any("database fingerprint unchanged" in msg for msg in messages)
-    assert calls["tick"] == 3
+    assert any("after 30s corrupt-board backoff" in msg for msg in messages)
+    assert calls["tick"] == 2
 
 
 # ---------------------------------------------------------------------------

tests/hermes_cli/test_kanban_dispatcher_resilience.py
@@ -1,0 +1,225 @@
+from __future__ import annotations
+
+import asyncio
+import inspect
+import logging
+import sqlite3
+import time
+from types import SimpleNamespace
+
+
+class _DummyConn:
+    def close(self) -> None:
+        pass
+
+
+def _install_gateway_dispatcher_harness(
+    monkeypatch,
+    tmp_path,
+    *,
+    dispatch_once,
+    stop_after_ticks: int,
+    monotonic_values: list[float] | None = None,
+    after_tick=None,
+):
+    from gateway.run import GatewayRunner
+    import hermes_cli.config as _cfg_mod
+    import hermes_cli.kanban_db as _kb
+
+    db_path = tmp_path / "kanban.db"
+    runner = object.__new__(GatewayRunner)
+    runner._running = True
+    calls = {"ticks": 0}
+
+    monkeypatch.setattr(
+        _cfg_mod,
+        "load_config",
+        lambda: {
+            "kanban": {
+                "dispatch_in_gateway": True,
+                "dispatch_interval_seconds": 1,
+                "auto_decompose": False,
+            }
+        },
+    )
+    monkeypatch.setattr(
+        _kb,
+        "list_boards",
+        lambda include_archived=False: [{"slug": _kb.DEFAULT_BOARD}],
+    )
+    monkeypatch.setattr(_kb, "read_board_metadata", lambda slug: {"slug": slug})
+    monkeypatch.setattr(_kb, "kanban_db_path", lambda board=None: db_path)
+    monkeypatch.setattr(_kb, "connect", lambda *args, **kwargs: _DummyConn())
+    monkeypatch.setattr(_kb, "dispatch_once", dispatch_once)
+
+    async def _to_thread(fn, *args, **kwargs):
+        name = getattr(fn, "__name__", "")
+        if name == "reap_worker_zombies":
+            return []
+        if name == "_ready_nonempty":
+            return False
+        result = fn(*args, **kwargs)
+        if name == "_tick_once":
+            calls["ticks"] += 1
+            if after_tick is not None:
+                after_tick(calls["ticks"])
+            if calls["ticks"] >= stop_after_ticks:
+                runner._running = False
+        return result
+
+    async def _sleep(_delay):
+        return None
+
+    monkeypatch.setattr("gateway.run.asyncio.to_thread", _to_thread)
+    monkeypatch.setattr("gateway.run.asyncio.sleep", _sleep)
+
+    if monotonic_values is not None:
+        values = iter(monotonic_values)
+        fallback = monotonic_values[-1]
+        real_monotonic = time.monotonic
+
+        def _monotonic_for_gateway_dispatcher():
+            caller = inspect.currentframe().f_back  # type: ignore[union-attr]
+            code = caller.f_code if caller is not None else None
+            filename = code.co_filename if code is not None else ""
+            function_name = code.co_name if code is not None else ""
+            if filename.replace("\\", "/").endswith(
+                "gateway/kanban_watchers.py"
+            ) and function_name in {
+                "_record_confirmed_corrupt_board",
+                "_tick_once_for_board",
+            }:
+                return next(values, fallback)
+            return real_monotonic()
+
+        monkeypatch.setattr(
+            "gateway.kanban_watchers.time.monotonic",
+            _monotonic_for_gateway_dispatcher,
+        )
+
+    return runner, db_path, calls
+
+
+def _run_watcher(runner) -> None:
+    asyncio.run(asyncio.wait_for(runner._kanban_dispatcher_watcher(), timeout=3.0))
+
+
+def test_gateway_dispatcher_quick_check_ok_does_not_latch(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=2,
+    )
+    with sqlite3.connect(db_path) as conn:
+        conn.execute("CREATE TABLE marker(id INTEGER)")
+
+    with caplog.at_level(logging.WARNING, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 2
+    assert dispatch_calls["count"] == 2
+    assert not any("not a valid SQLite database" in msg for msg in messages)
+    assert sum("read-only quick_check passed" in msg for msg in messages) == 2
+
+
+def test_gateway_dispatcher_confirmed_corruption_doubles_backoff(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=3,
+        monotonic_values=[1000.0, 1029.0, 1030.0, 1030.0],
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 3
+    assert dispatch_calls["count"] == 2
+    assert any("for 30s" in msg for msg in messages)
+    assert any("for 60s" in msg for msg in messages)
+    assert any("after 30s corrupt-board backoff" in msg for msg in messages)
+
+
+def test_gateway_dispatcher_fingerprint_change_retries_and_resets_backoff(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        raise sqlite3.DatabaseError("database disk image is malformed")
+
+    def _after_tick(tick: int) -> None:
+        if tick == 1:
+            db_path.write_text("not sqlite but changed", encoding="utf-8")
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=2,
+        monotonic_values=[1000.0, 1001.0, 1001.0],
+        after_tick=_after_tick,
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 2
+    assert dispatch_calls["count"] == 2
+    assert sum("for 30s" in msg for msg in messages) == 2
+    assert not any("for 60s" in msg for msg in messages)
+    assert any("database changed; retrying dispatch" in msg for msg in messages)
+
+
+def test_gateway_dispatcher_success_clears_disabled_board_state(
+    monkeypatch, tmp_path, caplog
+):
+    dispatch_calls = {"count": 0}
+
+    def _dispatch_once(*args, **kwargs):
+        dispatch_calls["count"] += 1
+        if dispatch_calls["count"] in {1, 3}:
+            raise sqlite3.DatabaseError("database disk image is malformed")
+        return SimpleNamespace(spawned=[])
+
+    runner, db_path, calls = _install_gateway_dispatcher_harness(
+        monkeypatch,
+        tmp_path,
+        dispatch_once=_dispatch_once,
+        stop_after_ticks=3,
+        monotonic_values=[1000.0, 1030.0, 1031.0],
+    )
+    db_path.write_text("not sqlite", encoding="utf-8")
+
+    with caplog.at_level(logging.INFO, logger="gateway.run"):
+        _run_watcher(runner)
+
+    messages = [record.getMessage() for record in caplog.records]
+    assert calls["ticks"] == 3
+    assert dispatch_calls["count"] == 3
+    assert sum("for 30s" in msg for msg in messages) == 2
+    assert not any("for 60s" in msg for msg in messages)
6ef5a975e4e7912f8016b9ab970e412c6e2e9a7c
Ho Lim <subhoya@gmail.com>
2026-07-11 05:48:38 -0700

    test(web): run app routing test in jsdom
    Signed-off-by: Ho Lim <subhoya@gmail.com>

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,3 +1,5 @@
+// @vitest-environment jsdom
+
 import { act, type ReactNode, useEffect } from "react";
 import { createRoot, type Root } from "react-dom/client";
 import { MemoryRouter } from "react-router-dom";

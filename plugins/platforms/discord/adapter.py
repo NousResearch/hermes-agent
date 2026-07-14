AnandaAnugrahHandyanto/savarez_agent

feat(gateway): add contextual rationale to approval prompts (#27604)
Salvage of #27833 by @zccyman, re-targeted onto current main: Discord moved to plugins adapter, WhatsApp Cloud adapter added, platform size budgets enforced. Original feature and approach by @zccyman.

plugins/platforms/discord/adapter.py
@@ -4432,10 +4432,15 @@ class DiscordAdapter(BasePlatformAdapter):
     async def send_exec_approval(
         self, chat_id: str, command: str, session_key: str,
         description: str = "dangerous command",
+        contextual_reason: str = "",
         metadata: Optional[dict] = None,
     ) -> SendResult:
         """
         Send a button-based exec approval prompt for a dangerous command.
+
+        ``contextual_reason`` (optional) is the agent's latest rationale; it is
+        rendered above the command (within the embed description budget) so the
+        user sees *why* approval is needed.
 
         The buttons call ``resolve_gateway_approval()`` to unblock the waiting
         agent thread — this replaces the text-based ``/approve`` flow on Discord.
@@ -4453,12 +4458,28 @@ class DiscordAdapter(BasePlatformAdapter):
             if not channel:
                 channel = await self._client.fetch_channel(int(target_id))
 
-            # Discord embed description limit is 4096; show full command up to that
+            # Discord embed description limit is 4096; budget the rationale +
+            # command so the combined description stays under it.  Reserve up
+            # to ~1500 chars for the rationale, then fit the command in what
+            # remains; truncate each with an ellipsis as needed.
             max_desc = 4088
-            cmd_display = command if len(command) <= max_desc else command[: max_desc - 3] + "..."
+            _rationale_block = ""
+            if contextual_reason:
+                _r = contextual_reason
+                if len(_r) > 1500:
+                    _r = _r[:1497] + "..."
+                _rationale_block = f"{_r}\n\n"
+            # Account for the rationale block and the ```\n ... \n``` fences.
+            _cmd_budget = max_desc - len(_rationale_block) - len("```\n\n```")
+            if _cmd_budget < 0:
+                _cmd_budget = 0
+            cmd_display = (
+                command if len(command) <= _cmd_budget
+                else command[: max(_cmd_budget - 3, 0)] + "..."
+            )
             embed = discord.Embed(
                 title="⚠️ Command Approval Required",
-                description=f"```\n{cmd_display}\n```",
+                description=f"{_rationale_block}```\n{cmd_display}\n```",
                 color=discord.Color.orange(),
             )
             embed.add_field(name="Reason", value=description, inline=False)

plugins/platforms/teams/adapter.py
@@ -1019,9 +1019,14 @@ class TeamsAdapter(BasePlatformAdapter):
         command: str,
         session_key: str,
         description: str = "dangerous command",
+        contextual_reason: str = "",
         metadata: Optional[Dict[str, Any]] = None,
     ) -> SendResult:
-        """Send an Adaptive Card approval prompt with Allow/Deny buttons."""
+        """Send an Adaptive Card approval prompt with Allow/Deny buttons.
+
+        ``contextual_reason`` (optional) is the agent's latest rationale; it is
+        rendered above the command so the user sees *why* approval is needed.
+        """
         if not self._app:
             return SendResult(success=False, error="Teams app not initialized")
 
@@ -1033,14 +1038,23 @@ class TeamsAdapter(BasePlatformAdapter):
             "desc": description,
         }
 
+        _body_blocks = [
+            TextBlock(text="⚠️ Command Approval Required", wrap=True, weight="Bolder"),
+        ]
+        if contextual_reason:
+            _r = contextual_reason
+            if len(_r) > 1500:
+                _r = _r[:1497] + "..."
+            _body_blocks.append(TextBlock(text=_r, wrap=True))
+        _body_blocks.extend([
+            TextBlock(text=f"```\n{cmd_preview}\n```", wrap=True),
+            TextBlock(text=f"Reason: {description}", wrap=True, isSubtle=True),
+        ])
+
         card = (
             AdaptiveCard()
             .with_version("1.4")
-            .with_body([
-                TextBlock(text="⚠️ Command Approval Required", wrap=True, weight="Bolder"),
-                TextBlock(text=f"```\n{cmd_preview}\n```", wrap=True),
-                TextBlock(text=f"Reason: {description}", wrap=True, isSubtle=True),
-            ])
+            .with_body(_body_blocks)
             .with_actions([
                 ExecuteAction(
                     title="Allow Once",

NousResearch/hermes-agent

plugins/platforms/discord/adapter.py
@@ -5570,12 +5570,17 @@ class DiscordAdapter(BasePlatformAdapter):
     async def send_exec_approval(
         self, chat_id: str, command: str, session_key: str,
         description: str = "dangerous command",
+        contextual_reason: str = "",
         metadata: Optional[dict] = None,
         allow_permanent: bool = True,
         smart_denied: bool = False,
     ) -> SendResult:
         """
         Send a button-based exec approval prompt for a dangerous command.
+
+        ``contextual_reason`` (optional) is the agent's latest rationale; it is
+        rendered above the command (within the embed description budget) so the
+        user sees *why* approval is needed.
 
         The buttons call ``resolve_gateway_approval()`` to unblock the waiting
         agent thread — this replaces the text-based ``/approve`` flow on Discord.
@@ -5603,8 +5608,16 @@ class DiscordAdapter(BasePlatformAdapter):
             if len(reason_display) > reason_budget:
                 reason_display = reason_display[: reason_budget - 15] + "... [truncated]"
 
+            rationale_plain = ""
+            if contextual_reason:
+                _r = str(contextual_reason)
+                if len(_r) > 800:
+                    _r = _r[:797] + "..."
+                rationale_plain = f"**Why:** {_r}\n\n"
+
             prompt_prefix = (
                 "⚠️ **Command Approval Required**\n\n"
+                f"{rationale_plain}"
                 "Do you want Hermes to run this command?\n\n"
                 "**Requested command:**\n```bash\n"
             )
@@ -5625,14 +5638,27 @@ class DiscordAdapter(BasePlatformAdapter):
             content = f"{prompt_prefix}{content_cmd_display}{prompt_tail}"
 
             # Preserve the richer embed path and its larger description budget
-            # for clients where embeds render correctly.
+            # for clients where embeds render correctly. Budget rationale +
+            # command so the embed description stays under Discord's 4096-char
+            # limit (use 4088 as a safety margin).
             max_embed_desc = 4088
+            _rationale_block = ""
+            if contextual_reason:
+                _r = str(contextual_reason)
+                if len(_r) > 1500:
+                    _r = _r[:1497] + "..."
+                _rationale_block = f"{_r}\n\n"
+            _cmd_budget = max_embed_desc - len(_rationale_block) - len("```\n\n```")
+            if _cmd_budget < 0:
+                _cmd_budget = 0
             embed_cmd_display = str(command or "")
-            if len(embed_cmd_display) > max_embed_desc:
-                embed_cmd_display = embed_cmd_display[: max_embed_desc - 3] + "..."
+            if len(embed_cmd_display) > _cmd_budget:
+                embed_cmd_display = (
+                    embed_cmd_display[: max(0, _cmd_budget - 3)] + "..."
+                )
             embed = discord.Embed(
                 title="⚠️ Command Approval Required",
-                description=f"```\n{embed_cmd_display}\n```",
+                description=f"{_rationale_block}```\n{embed_cmd_display}\n```",
                 color=discord.Color.orange(),
             )
             embed.add_field(name="Reason", value=reason_display, inline=False)

plugins/platforms/teams/adapter.py
@@ -1097,11 +1097,16 @@ class TeamsAdapter(BasePlatformAdapter):
         command: str,
         session_key: str,
         description: str = "dangerous command",
+        contextual_reason: str = "",
         metadata: Optional[Dict[str, Any]] = None,
         allow_permanent: bool = True,
         smart_denied: bool = False,
     ) -> SendResult:
-        """Send an Adaptive Card approval prompt with Allow/Deny buttons."""
+        """Send an Adaptive Card approval prompt with Allow/Deny buttons.
+
+        ``contextual_reason`` (optional) is the agent's latest rationale; it is
+        rendered above the command so the user sees *why* approval is needed.
+        """
         if not self._app:
             return SendResult(success=False, error="Teams app not initialized")
 
@@ -1133,9 +1138,16 @@ class TeamsAdapter(BasePlatformAdapter):
         ))
         body = [
             TextBlock(text="⚠️ Command Approval Required", wrap=True, weight="Bolder"),
+        ]
+        if contextual_reason:
+            _r = contextual_reason
+            if len(_r) > 1500:
+                _r = _r[:1497] + "..."
+            body.append(TextBlock(text=_r, wrap=True))
+        body.extend([
             TextBlock(text=f"```\n{cmd_preview}\n```", wrap=True),
             TextBlock(text=f"Reason: {description}", wrap=True, isSubtle=True),
-        ]
+        ])
         if smart_denied:
             body.append(TextBlock(
                 text="Smart DENY: owner override applies to this one operation only.", wrap=True
@@ -1149,6 +1161,7 @@ class TeamsAdapter(BasePlatformAdapter):
         except Exception as e:
             logger.error("[teams] send_exec_approval failed: %s", e, exc_info=True)
             return SendResult(success=False, error=str(e), retryable=True)
+
 
     async def send(
         self,
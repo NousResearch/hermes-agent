AnandaAnugrahHandyanto/savarez_agent

fix(nix): add opt-in local browser runtime

nix/packages.nix
@@ -54,6 +54,11 @@
         fix-lockfiles = hermesAgent.hermesNpmLib.mkFixLockfiles {
           packages = [ hermesAgent.hermesTui hermesAgent.hermesWeb hermesAgent.hermesDesktop ];
         };
+      } // lib.optionalAttrs pkgs.stdenv.isLinux {
+        # Local browser tools with the Nixpkgs agent-browser CLI and Chromium.
+        browser = hermesAgent.override {
+          withBrowser = true;
+        };
       };
     };
 }

NousResearch/hermes-agent

nix/packages.nix
@@ -58,6 +58,11 @@
         tui = full.hermesTui;
         web = full.hermesWeb;
         desktop = full.hermesDesktop;
+      } // lib.optionalAttrs pkgs.stdenv.isLinux {
+        # Local browser tools with the Nixpkgs agent-browser CLI and Chromium.
+        browser = full.override {
+          withBrowser = true;
+        };
       };
     };
 }
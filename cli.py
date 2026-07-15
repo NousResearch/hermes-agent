AnandaAnugrahHandyanto/savarez_agent

fix(cli): use rich.Table for resume session list — fix CJK alignment
Replace basic str.format() column padding with rich.table.Table which
properly handles CJK (double-width) characters. Also rearrange columns
so that 'Last Active' and 'ID' appear on the left side, keeping the
variable-length 'Title / Preview' on the right where width doesn't
affect alignment of other columns.

Changes:
- cli.py (_show_recent_sessions): switch from print(f'{:32}') to
  rich.table.Table with CJK-aware column widths; merge Title + Preview
  into a single column; increase preview width from 38 to 80 chars.
- hermes_cli/main.py (cmd_sessions 'list'): same treatment for the
  'hermes sessions list' command — rich.table.Table with columns
  reordered to Last Active | ID | Title | Preview.

cli.py
@@ -5726,6 +5726,7 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
             return False
 
         from hermes_cli.main import _relative_time
+        from rich.table import Table
 
         print()
         if reason == "history":
@@ -5733,13 +5734,21 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
         else:
             print("  Recent sessions:")
         print()
-        print(f"  {'#':<3} {'Title':<32} {'Preview':<40} {'Last Active':<13} {'ID'}")
-        print(f"  {'─' * 3} {'─' * 32} {'─' * 40} {'─' * 13} {'─' * 24}")
+
+        table = Table(show_header=True, header_style="bold", box=None, padding=(0, 1))
+        table.add_column("#", justify="right", width=3, no_wrap=True)
+        table.add_column("Last Active", width=13, no_wrap=True)
+        table.add_column("ID", width=24, no_wrap=True)
+        table.add_column("Title / Preview")
+
         for idx, session in enumerate(sessions, start=1):
             title = session.get("title") or "—"
-            preview = (session.get("preview") or "")[:38]
+            preview = (session.get("preview") or "")[:80]
+            combined = f"{title} — {preview}" if preview else title
             last_active = _relative_time(session.get("last_active"))
-            print(f"  {idx:<3} {title:<32} {preview:<40} {last_active:<13} {session['id']}")
+            table.add_row(str(idx), last_active, session["id"], combined)
+
+        Console().print(table)
         print()
         print("  Use /resume <number>, /resume <session id>, or /resume <session title> to continue.")
         print("  Example: /resume 2")

NousResearch/hermes-agent

cli.py
@@ -6834,15 +6834,25 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
         if reason == "history":
             _cli_visible_print("(._.) No messages in the current chat yet — here are recent sessions you can resume:")
         else:
+else:
             _cli_visible_print("  Recent sessions:")
         _cli_visible_print()
-        _cli_visible_print(f"  {'#':<3} {'Title':<32} {'Preview':<40} {'Last Active':<13} {'ID'}")
-        _cli_visible_print(f"  {'─' * 3} {'─' * 32} {'─' * 40} {'─' * 13} {'─' * 24}")
+        from rich.table import Table
+
+        table = Table(show_header=True, header_style="bold", box=None, padding=(0, 1))
+        table.add_column("#", justify="right", width=3, no_wrap=True)
+        table.add_column("Last Active", width=13, no_wrap=True)
+        table.add_column("ID", width=24, no_wrap=True)
+        table.add_column("Title / Preview")
+
         for idx, session in enumerate(sessions, start=1):
             title = session.get("title") or "—"
-            preview = (session.get("preview") or "")[:38]
+            preview = (session.get("preview") or "")[:80]
+            combined = f"{title} — {preview}" if preview else title
             last_active = _relative_time(session.get("last_active"))
-            _cli_visible_print(f"  {idx:<3} {title:<32} {preview:<40} {last_active:<13} {session['id']}")
+            table.add_row(str(idx), last_active, session["id"], combined)
+
+        ChatConsole().print(table)
         _cli_visible_print()
         _cli_visible_print("  Use /resume <number>, /resume <session id>, or /resume <session title> to continue.")
         _cli_visible_print("  Example: /resume 2")
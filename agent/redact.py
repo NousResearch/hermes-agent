ai-integr8tor/NousResearch-hermes-agent

feat(redact): validators, PII patterns, and redact_level config
Add Shannon entropy + Luhn validators (ported from @nacho-labs/nachos-dlp)
to reduce false positives, extend _PREFIX_PATTERNS with 7 previously-missing
providers (Twilio, Mailchimp, Discord bot, Azure Storage, Stripe webhook),
and introduce security.redact_level (basic|standard|strict) for opt-in PII
coverage:

  basic    — existing behaviour, no change (default)
  standard — + credit cards (Luhn-validated), US SSNs, IBANs
  strict   — + email addresses, non-loopback IPv4 addresses

The redact_level value is bridged from config.yaml to HERMES_REDACT_LEVEL
at startup in hermes_cli/main.py and gateway/run.py, using the same
import-time snapshot pattern as redact_secrets to prevent mid-session
tampering.

100 tests pass (tests/agent/test_redact.py + tests/hermes_cli/test_redact_config_bridge.py).

agent/redact.py
@@ -5,9 +5,13 @@ before they reach log files, verbose output, or gateway logs.
 
 Short tokens (< 18 chars) are fully masked. Longer tokens preserve
 the first 6 and last 4 characters for debuggability.
+
+Supports ``basic``, ``standard``, and ``strict`` redaction levels via
+``security.redact_level`` / ``HERMES_REDACT_LEVEL``.
 """
 
 import logging
+import math
 import os
 import re
 
@@ -66,6 +70,11 @@ _SENSITIVE_BODY_KEYS = frozenset({
 # downgrade — see `_log_redaction_status()` in gateway/run.py and cli.py.
 _REDACT_ENABLED = os.getenv("HERMES_REDACT_SECRETS", "true").lower() in {"1", "true", "yes", "on"}
 
+# Snapshotted at import time alongside _REDACT_ENABLED.
+_REDACT_LEVEL = os.getenv("HERMES_REDACT_LEVEL", "basic").lower().strip()
+if _REDACT_LEVEL not in {"basic", "standard", "strict"}:
+    _REDACT_LEVEL = "basic"
+
 # Known API key prefixes -- match the prefix + contiguous token chars
 _PREFIX_PATTERNS = [
     r"sk-[A-Za-z0-9_-]{10,}",           # OpenAI / OpenRouter / Anthropic (sk-ant-*)
@@ -104,6 +113,13 @@ _PREFIX_PATTERNS = [
     r"mem0_[A-Za-z0-9]{10,}",           # Mem0 Platform API key
     r"brv_[A-Za-z0-9]{10,}",            # ByteRover API key
     r"xai-[A-Za-z0-9]{30,}",            # xAI (Grok) API key
+    r"AC[A-Za-z0-9]{32}",               # Twilio Account SID
+    r"SK[A-Za-z0-9]{32}",               # Twilio Auth Token / API Key
+    r"[A-Za-z0-9]{32}-us[0-9]{1,2}",    # Mailchimp API key (<key>-usN)
+    r"Bot\.[A-Za-z0-9_-]{24}\.[A-Za-z0-9_-]{6}\.[A-Za-z0-9_-]{27}",  # Discord bot token
+    r"[A-Za-z0-9+/]{88}",               # Azure Storage Account Key (base64, 88 chars)
+    r"whsec_[A-Za-z0-9+/]{32,}",        # Stripe webhook signing secret
+    r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}:[-A-Za-z0-9+/=]{20,}",  # Azure SAS sig
 ]
 
 # ENV assignment patterns: KEY=value where KEY contains a secret-like name
@@ -432,6 +448,11 @@ def redact_sensitive_text(text: str, *, force: bool = False, code_file: bool = F
             return phone[:4] + "****" + phone[-4:]
         text = _SIGNAL_PHONE_RE.sub(_redact_phone, text)
 
+    if _REDACT_LEVEL in {"standard", "strict"}:
+        text = _redact_pii(text)
+        if _REDACT_LEVEL == "strict":
+            text = _redact_strict_pii(text)
+
     return text
 
 
@@ -491,6 +512,109 @@ def _has_http_method_substring(text: str) -> bool:
     """Cheap pre-check before scanning for access-log request targets."""
     upper = text.upper()
     return any(method in upper for method in _HTTP_METHOD_SUBSTRINGS)
+
+
+def _shannon_entropy(value: str) -> float:
+    """Calculate Shannon entropy in bits per character."""
+    if not value:
+        return 0.0
+    freq: dict[str, int] = {}
+    for ch in value:
+        freq[ch] = freq.get(ch, 0) + 1
+    length = len(value)
+    return -sum((c / length) * math.log2(c / length) for c in freq.values())
+
+
+def _luhn_valid(number: str) -> bool:
+    """Validate a credit-card string with the Luhn algorithm."""
+    digits = "".join(c for c in number if c.isdigit())
+    if len(digits) < 13 or len(digits) > 19:
+        return False
+    total = 0
+    reverse = digits[::-1]
+    for i, d in enumerate(reverse):
+        n = int(d)
+        if i % 2 == 1:
+            n *= 2
+            if n > 9:
+                n -= 9
+        total += n
+    return total % 10 == 0
+
+
+_CREDIT_CARD_RE = re.compile(
+    r"(?<!\d)"
+    r"(\d{4}[-\s]?\d{4}[-\s]?\d{4}[-\s]?\d{1,4}"  # 16-digit: Visa/MC/Discover
+    r"|3[47]\d{2}[-\s]?\d{6}[-\s]?\d{5}"           # 15-digit: Amex
+    r"|3(?:0[0-5]|[68]\d)\d{11}"                    # 14-digit: Diners
+    r")"
+    r"(?!\d)"
+)
+
+_SSN_RE = re.compile(
+    r"(?<!\d)"
+    r"(?!000|666|9\d\d)"      # AAA block exclusions
+    r"(\d{3})"
+    r"[-\s]"
+    r"(?!00)"
+    r"(\d{2})"
+    r"[-\s]"
+    r"(?!0000)"
+    r"(\d{4})"
+    r"(?!\d)"
+)
+
+_IBAN_RE = re.compile(
+    r"\b([A-Z]{2}\d{2}[A-Z0-9]{4,30}|[A-Z]{2}\d{2}(?:\s[A-Z0-9]{4})+(?:\s[A-Z0-9]{1,4})?)\b"
+)
+
+_EMAIL_RE = re.compile(
+    r"(?<![{<])\b[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}\b(?![}>])"
+)
+
+_IPV4_RE = re.compile(
+    r"(?<!\d)"
+    r"((?!0\.|127\.|169\.254\.|255\.255\.255\.255)"
+    r"(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}"
+    r"(?:25[0-5]|2[0-4]\d|[01]?\d\d?))"
+    r"(?!\d)"
+)
+
+
+def _redact_pii(text: str) -> str:
+    """Apply standard-level PII redaction."""
+    def _redact_cc(m: re.Match) -> str:
+        raw = m.group(0)
+        digits = "".join(c for c in raw if c.isdigit())
+        if _luhn_valid(digits):
+            if len(digits) < 18:
+                return "****-****-****-" + digits[-4:]
+            return "***" + digits[-4:]
+        return raw
+
+    if any(c.isdigit() for c in text):
+        text = _CREDIT_CARD_RE.sub(_redact_cc, text)
+
+    if "-" in text or " " in text:
+        text = _SSN_RE.sub(r"***-**-\3", text)
+
+    if any(c.isalpha() and c.isupper() for c in text):
+        text = _IBAN_RE.sub(lambda m: m.group(0)[:4] + "****", text)
+
+    return text
+
+
+def _redact_strict_pii(text: str) -> str:
+    """Apply strict-level PII redaction."""
+    if "@" in text:
+        text = _EMAIL_RE.sub(lambda m: m.group(0).split("@")[0][:2] + "***@***", text)
+
+    if "." in text:
+        text = _IPV4_RE.sub(
+            lambda m: ".".join(m.group(1).split(".")[:2]) + ".***.***", text
+        )
+
+    return text
 
 
 class RedactingFormatter(logging.Formatter):

hermes_cli/main.py
@@ -331,6 +331,13 @@ try:
                 _early_redact = _early_sec_cfg.get("redact_secrets")
                 if _early_redact is not None:
                     os.environ["HERMES_REDACT_SECRETS"] = str(_early_redact).lower()
+        # Bridge security.redact_level early, like redact_secrets.
+        if "HERMES_REDACT_LEVEL" not in os.environ:
+            _early_sec_cfg2 = _early_cfg_raw.get("security", {})
+            if isinstance(_early_sec_cfg2, dict):
+                _early_level = _early_sec_cfg2.get("redact_level")
+                if _early_level is not None:
+                    os.environ["HERMES_REDACT_LEVEL"] = str(_early_level).lower()
         _early_net_cfg = _early_cfg_raw.get("network", {})
         if isinstance(_early_net_cfg, dict) and _early_net_cfg.get("force_ipv4"):
             _FORCE_IPV4_EARLY = True

NousResearch/hermes-agent

agent/redact.py
@@ -67,6 +67,11 @@ _SENSITIVE_BODY_KEYS = frozenset({
 # downgrade — see `_log_redaction_status()` in gateway/run.py and cli.py.
 _REDACT_ENABLED = os.getenv("HERMES_REDACT_SECRETS", "true").lower() in {"1", "true", "yes", "on"}
 
+# Snapshot optional PII coverage at import time alongside _REDACT_ENABLED.
+_REDACT_LEVEL = os.getenv("HERMES_REDACT_LEVEL", "basic").lower().strip()
+if _REDACT_LEVEL not in {"basic", "standard", "strict"}:
+    _REDACT_LEVEL = "basic"
+
 # Known API key prefixes -- match the prefix + contiguous token chars
 _PREFIX_PATTERNS = [
     r"sk-[A-Za-z0-9_-]{10,}",           # OpenAI / OpenRouter / Anthropic (sk-ant-*)
@@ -110,7 +115,15 @@ _PREFIX_PATTERNS = [
     r"fw-[A-Za-z0-9]{30,}",             # Fireworks AI API key
     r"fw_[A-Za-z0-9]{30,}",             # Fireworks AI API key
     r"fpk_[A-Za-z0-9]{30,}",            # Fireworks AI project key
+    r"AC[A-Za-z0-9]{32}",               # Twilio Account SID
+    r"SK[A-Za-z0-9]{32}",               # Twilio API key
+    r"Bot\.[A-Za-z0-9_-]{24}\.[A-Za-z0-9_-]{6}\.[A-Za-z0-9_-]{27}",  # Discord bot token
+    r"whsec_[A-Za-z0-9+/]{32,}",        # Stripe webhook signing secret
 ]
+
+# Mailchimp tokens have no vendor prefix. Keep this out of _PREFIX_PATTERNS so
+# the inexpensive prefix gate remains effective for ordinary text.
+_MAILCHIMP_API_KEY_RE = re.compile(r"[A-Za-z0-9]{32}-us[0-9]{1,2}\b")
 
 # ENV assignment patterns: KEY=value where KEY contains a secret-like name.
 # Uppercase keys tolerate spaces around "=" (e.g. ``FOO_SECRET = bar``) because
@@ -546,6 +559,11 @@ def redact_sensitive_text(
         _prefix_sub = _mask_token_nonreusable if file_read else _mask_token
         text = _PREFIX_RE.sub(lambda m: _prefix_sub(m.group(1)), text)
 
+    # Mailchimp keys are structurally identifiable but lack a vendor prefix.
+    # Gate on their datacenter suffix instead of scanning every log line.
+    if "-us" in text:
+        text = _MAILCHIMP_API_KEY_RE.sub(lambda m: _mask_token(m.group(0)), text)
+
     # ENV assignments: OPENAI_API_KEY=***  (skip for code files — false positives)
     if not code_file:
         if "=" in text:
@@ -679,6 +697,11 @@ def redact_sensitive_text(
             return phone[:4] + "****" + phone[-4:]
         text = _SIGNAL_PHONE_RE.sub(_redact_phone, text)
 
+    if _REDACT_LEVEL in {"standard", "strict"}:
+        text = _redact_standard_pii(text)
+    if _REDACT_LEVEL == "strict":
+        text = _redact_strict_pii(text)
+
     return text
 
 
@@ -798,6 +821,50 @@ def _has_http_method_substring(text: str) -> bool:
     """Cheap pre-check before scanning for access-log request targets."""
     upper = text.upper()
     return any(method in upper for method in _HTTP_METHOD_SUBSTRINGS)
+
+
+_CREDIT_CARD_RE = re.compile(
+    r"(?<!\d)(\d{4}[-\s]?\d{4}[-\s]?\d{4}[-\s]?\d{1,4}|"
+    r"3[47]\d{2}[-\s]?\d{6}[-\s]?\d{5}|3(?:0[0-5]|[68]\d)\d{11})(?!\d)"
+)
+_SSN_RE = re.compile(
+    r"(?<!\d)(?!000|666|9\d\d)(\d{3})[-\s](?!00)(\d{2})[-\s](?!0000)(\d{4})(?!\d)"
+)
+_IBAN_RE = re.compile(r"\b([A-Z]{2}\d{2}[A-Z0-9]{4,30})\b")
+_EMAIL_RE = re.compile(r"\b[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}\b")
+_IPV4_RE = re.compile(
+    r"(?<!\d)((?!0\.|127\.|169\.254\.)"
+    r"(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?))(?!\d)"
+)
+
+
+def _luhn_valid(number: str) -> bool:
+    """Return whether a 13-19 digit payment-card candidate passes Luhn."""
+    digits = "".join(character for character in number if character.isdigit())
+    if not 13 <= len(digits) <= 19:
+        return False
+    total = 0
+    for index, character in enumerate(reversed(digits)):
+        value = int(character)
+        if index % 2:
+            value = value * 2 - 9 if value > 4 else value * 2
+        total += value
+    return total % 10 == 0
+
+
+def _redact_standard_pii(text: str) -> str:
+    def redact_card(match: re.Match) -> str:
+        digits = "".join(character for character in match.group(0) if character.isdigit())
+        return "****-****-****-" + digits[-4:] if _luhn_valid(digits) else match.group(0)
+
+    text = _CREDIT_CARD_RE.sub(redact_card, text)
+    text = _SSN_RE.sub(r"***-**-\3", text)
+    return _IBAN_RE.sub(lambda match: match.group(0)[:4] + "****", text)
+
+
+def _redact_strict_pii(text: str) -> str:
+    text = _EMAIL_RE.sub(lambda match: match.group(0).split("@", 1)[0][:2] + "***@***", text)
+    return _IPV4_RE.sub(lambda match: ".".join(match.group(1).split(".")[:2]) + ".***.***", text)
 
 
 class RedactingFormatter(logging.Formatter):

hermes_cli/main.py
@@ -569,6 +569,12 @@ try:
                 _early_redact = _early_sec_cfg.get("redact_secrets")
                 if _early_redact is not None:
                     os.environ["HERMES_REDACT_SECRETS"] = str(_early_redact).lower()
+        if "HERMES_REDACT_LEVEL" not in os.environ:
+            _early_sec_cfg = _early_cfg_raw.get("security", {})
+            if isinstance(_early_sec_cfg, dict):
+                _early_level = _early_sec_cfg.get("redact_level")
+                if _early_level is not None:
+                    os.environ["HERMES_REDACT_LEVEL"] = str(_early_level).lower()
         _early_net_cfg = _early_cfg_raw.get("network", {})
         if isinstance(_early_net_cfg, dict) and _early_net_cfg.get("force_ipv4"):
             _FORCE_IPV4_EARLY = True
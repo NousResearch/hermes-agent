AnandaAnugrahHandyanto/savarez_agent

fix(mattermost): make post length configurable
Mattermost supports larger posts than Hermes' legacy 4,000-character chunk
limit, which can make long threaded replies noisier than necessary on servers
that tolerate larger messages. Add a mattermost.max_post_length setting, clamp
it to Mattermost's 16,383-character cap, and keep the legacy 4,000-character
default for existing deployments.

Resolve the effective limit for both gateway adapter sends and direct
send_message/cron delivery so all Mattermost outbound paths chunk consistently.
Tests cover config/env resolution, clamping, tiny invalid limits, adapter sends,
and send_message chunking.

tests/gateway/test_mattermost.py
@@ -182,18 +182,45 @@ class TestMattermostConfigLoading:
         assert Platform.MATTERMOST in config.platforms
         assert config.platforms[Platform.MATTERMOST].extra.get("url") == ""
 
+    def test_config_yaml_mattermost_max_post_length_reaches_platform_extra(
+        self, monkeypatch, tmp_path
+    ):
+        """Top-level config.yaml Mattermost settings should seed runtime extras."""
+        monkeypatch.setenv("HERMES_HOME", str(tmp_path))
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        (tmp_path / "config.yaml").write_text(
+            "mattermost:\n"
+            "  enabled: true\n"
+            "  url: https://mm.example.com\n"
+            "  token: mm-tok-abc123\n"
+            "  max_post_length: 16000\n",
+            encoding="utf-8",
+        )
+
+        from gateway.config import load_gateway_config
+
+        config = load_gateway_config()
+
+        mattermost = config.platforms[Platform.MATTERMOST]
+        assert mattermost.enabled is True
+        assert mattermost.extra.get("max_post_length") == 16000
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") == "16000"
+
 
 # ---------------------------------------------------------------------------
 # Adapter format / truncate
 # ---------------------------------------------------------------------------
 
-def _make_adapter():
+def _make_adapter(extra=None):
     """Create a MattermostAdapter with mocked config."""
     from plugins.platforms.mattermost.adapter import MattermostAdapter
+    adapter_extra = {"url": "https://mm.example.com"}
+    if extra:
+        adapter_extra.update(extra)
     config = PlatformConfig(
         enabled=True,
         token="test-token",
-        extra={"url": "https://mm.example.com"},
+        extra=adapter_extra,
     )
     adapter = MattermostAdapter(config)
     return adapter
@@ -261,6 +288,99 @@ class TestMattermostTruncateMessage:
         msg = "x" * 4000
         chunks = self.adapter.truncate_message(msg, 4000)
         assert len(chunks) == 1
+
+    def test_configured_max_post_length_exposed_to_streaming(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 16000})
+        assert adapter.max_post_length == 16000
+        assert adapter.MAX_MESSAGE_LENGTH == 16000
+
+    def test_max_post_length_clamped_to_mattermost_server_limit(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 999999})
+        assert adapter.max_post_length == 16383
+        assert adapter.MAX_MESSAGE_LENGTH == 16383
+
+    def test_tiny_max_post_length_falls_back_to_safe_default(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 1})
+        assert adapter.max_post_length == 4000
+        assert adapter.MAX_MESSAGE_LENGTH == 4000
+
+    def test_tiny_env_max_post_length_falls_back_to_safe_default(self, monkeypatch):
+        monkeypatch.setenv("MATTERMOST_MAX_POST_LENGTH", "1")
+        adapter = _make_adapter({"max_post_length": 16000})
+        assert adapter.max_post_length == 4000
+
+    def test_env_max_post_length_overrides_config_extra(self, monkeypatch):
+        monkeypatch.setenv("MATTERMOST_MAX_POST_LENGTH", "16000")
+        adapter = _make_adapter({"max_post_length": 4000})
+        assert adapter.max_post_length == 16000
+
+    def test_apply_yaml_config_maps_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from plugins.platforms.mattermost.adapter import _apply_yaml_config
+
+        seeded = _apply_yaml_config({}, {"max_post_length": 16000})
+
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") == "16000"
+        assert seeded == {"max_post_length": 16000}
+
+    def test_apply_yaml_config_tiny_max_post_length_seeds_safe_default(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from plugins.platforms.mattermost.adapter import _apply_yaml_config
+
+        seeded = _apply_yaml_config({}, {"max_post_length": 1})
+
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") == "4000"
+        assert seeded == {"max_post_length": 4000}
+
+    @pytest.mark.asyncio
+    async def test_send_uses_configured_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 16000})
+        adapter._post_preserving_thread = AsyncMock(
+            return_value={"id": "post-1"}
+        )
+
+        result = await adapter.send("channel-id", "x" * 12000)
+
+        assert result.success is True
+        assert adapter._post_preserving_thread.call_count == 1
+        payload = adapter._post_preserving_thread.call_args.args[1]
+        assert payload["message"] == "x" * 12000
+
+    @pytest.mark.asyncio
+    async def test_send_message_tool_uses_configured_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from tools import send_message_tool
+
+        sent_chunks = []
+
+        async def fake_send_via_adapter(
+            platform,
+            pconfig,
+            chat_id,
+            message,
+            thread_id=None,
+            media_files=None,
+            force_document=False,
+        ):
+            sent_chunks.append(message)
+            return {"success": True, "message_id": f"post-{len(sent_chunks)}"}
+
+        monkeypatch.setattr(send_message_tool, "_send_via_adapter", fake_send_via_adapter)
+        pconfig = PlatformConfig(enabled=True, extra={"max_post_length": 16000})
+
+        result = await send_message_tool._send_to_platform(
+            Platform.MATTERMOST,
+            pconfig,
+            "channel-id",
+            "x" * 12000,
+        )
+
+        assert result == {"success": True, "message_id": "post-1"}
+        assert sent_chunks == ["x" * 12000]
 
 
 # ---------------------------------------------------------------------------

NousResearch/hermes-agent

tests/gateway/test_mattermost.py
@@ -183,18 +183,45 @@ class TestMattermostConfigLoading:
         assert Platform.MATTERMOST in config.platforms
         assert config.platforms[Platform.MATTERMOST].extra.get("url") == ""
 
+    def test_config_yaml_mattermost_max_post_length_reaches_platform_extra(
+        self, monkeypatch, tmp_path
+    ):
+        """Top-level config.yaml Mattermost settings should seed runtime extras."""
+        monkeypatch.setenv("HERMES_HOME", str(tmp_path))
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        (tmp_path / "config.yaml").write_text(
+            "mattermost:\n"
+            "  enabled: true\n"
+            "  url: https://mm.example.com\n"
+            "  token: mm-tok-abc123\n"
+            "  max_post_length: 16000\n",
+            encoding="utf-8",
+        )
+
+        from gateway.config import load_gateway_config
+
+        config = load_gateway_config()
+
+        mattermost = config.platforms[Platform.MATTERMOST]
+        assert mattermost.enabled is True
+        assert mattermost.extra.get("max_post_length") == 16000
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") is None
+
 
 # ---------------------------------------------------------------------------
 # Adapter format / truncate
 # ---------------------------------------------------------------------------
 
-def _make_adapter():
+def _make_adapter(extra=None):
     """Create a MattermostAdapter with mocked config."""
     from plugins.platforms.mattermost.adapter import MattermostAdapter
+    adapter_extra = {"url": "https://mm.example.com"}
+    if extra:
+        adapter_extra.update(extra)
     config = PlatformConfig(
         enabled=True,
         token="test-token",
-        extra={"url": "https://mm.example.com"},
+        extra=adapter_extra,
     )
     adapter = MattermostAdapter(config)
     return adapter
@@ -262,6 +289,117 @@ class TestMattermostTruncateMessage:
         msg = "x" * 4000
         chunks = self.adapter.truncate_message(msg, 4000)
         assert len(chunks) == 1
+
+    def test_configured_max_post_length_exposed_to_streaming(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 16000})
+        assert adapter.max_post_length == 16000
+        assert adapter.MAX_MESSAGE_LENGTH == 16000
+
+    def test_max_post_length_clamped_to_mattermost_server_limit(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 999999})
+        assert adapter.max_post_length == 16383
+        assert adapter.MAX_MESSAGE_LENGTH == 16383
+
+    def test_tiny_max_post_length_falls_back_to_safe_default(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 1})
+        assert adapter.max_post_length == 4000
+        assert adapter.MAX_MESSAGE_LENGTH == 4000
+
+    @pytest.mark.parametrize("value", ["not-a-number", float("inf"), float("-inf")])
+    def test_invalid_max_post_length_falls_back_to_safe_default(
+        self, monkeypatch, value
+    ):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": value})
+        assert adapter.max_post_length == 4000
+        assert adapter.MAX_MESSAGE_LENGTH == 4000
+
+    def test_environment_does_not_configure_post_length(self, monkeypatch):
+        monkeypatch.setenv("MATTERMOST_MAX_POST_LENGTH", "16000")
+        adapter = _make_adapter({})
+        assert adapter.max_post_length == 4000
+
+    def test_config_max_post_length_ignores_environment_value(self, monkeypatch):
+        monkeypatch.setenv("MATTERMOST_MAX_POST_LENGTH", "16000")
+        adapter = _make_adapter({"max_post_length": 4000})
+        assert adapter.max_post_length == 4000
+
+    def test_apply_yaml_config_maps_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from plugins.platforms.mattermost.adapter import _apply_yaml_config
+
+        seeded = _apply_yaml_config({}, {"max_post_length": 16000})
+
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") is None
+        assert seeded == {"max_post_length": 16000}
+
+    def test_apply_yaml_config_does_not_mutate_environment(self, monkeypatch):
+        monkeypatch.setenv("MATTERMOST_MAX_POST_LENGTH", "16000")
+        from plugins.platforms.mattermost.adapter import _apply_yaml_config
+
+        seeded = _apply_yaml_config({}, {"max_post_length": 8000})
+
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") == "16000"
+        assert seeded == {"max_post_length": 8000}
+
+    def test_apply_yaml_config_tiny_max_post_length_seeds_safe_default(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from plugins.platforms.mattermost.adapter import _apply_yaml_config
+
+        seeded = _apply_yaml_config({}, {"max_post_length": 1})
+
+        assert os.getenv("MATTERMOST_MAX_POST_LENGTH") is None
+        assert seeded == {"max_post_length": 4000}
+
+    @pytest.mark.asyncio
+    async def test_send_uses_configured_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        adapter = _make_adapter({"max_post_length": 16000})
+        adapter._post_preserving_thread = AsyncMock(
+            return_value={"id": "post-1"}
+        )
+
+        result = await adapter.send("channel-id", "x" * 12000)
+
+        assert result.success is True
+        assert adapter._post_preserving_thread.call_count == 1
+        payload = adapter._post_preserving_thread.call_args.args[1]
+        assert payload["message"] == "x" * 12000
+
+    @pytest.mark.asyncio
+    async def test_send_message_tool_uses_configured_max_post_length(self, monkeypatch):
+        monkeypatch.delenv("MATTERMOST_MAX_POST_LENGTH", raising=False)
+        from tools import send_message_tool
+
+        sent_chunks = []
+
+        async def fake_send_via_adapter(
+            platform,
+            pconfig,
+            chat_id,
+            message,
+            thread_id=None,
+            media_files=None,
+            force_document=False,
+        ):
+            sent_chunks.append(message)
+            return {"success": True, "message_id": f"post-{len(sent_chunks)}"}
+
+        monkeypatch.setattr(send_message_tool, "_send_via_adapter", fake_send_via_adapter)
+        pconfig = PlatformConfig(enabled=True, extra={"max_post_length": 16000})
+
+        result = await send_message_tool._send_to_platform(
+            Platform.MATTERMOST,
+            pconfig,
+            "channel-id",
+            "x" * 12000,
+        )
+
+        assert result == {"success": True, "message_id": "post-1"}
+        assert sent_chunks == ["x" * 12000]
 
 
 # ---------------------------------------------------------------------------
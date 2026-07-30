bottombgr-design/hermes

feat(tui): add exact code block copying

hermes_cli/cli_commands_mixin.py
@@ -2838,3 +2838,110 @@ class CLICommandsMixin:
         else:
             _cprint(f"Unknown voice subcommand: {subcommand}")
             _cprint("Usage: /voice [on|off|tts|status]")
+
+    def _handle_copy_code_command(self, cmd_original: str) -> None:
+        """Handle /copy-code [block_index] — pick or copy a fenced code block.
+
+        One block: copies immediately.
+        Multiple blocks: prints numbered list with language and preview.
+        With index (e.g. /copy-code 2): copies that block directly.
+
+        Searches backward through assistant messages for the most recent
+        response containing fenced code.
+        """
+        from cli import _cprint, _assistant_content_as_text
+        from hermes_cli.code_fences import parse_code_fences
+
+        parts = cmd_original.split(maxsplit=1)
+        raw_arg = parts[1].strip() if len(parts) > 1 else ""
+
+        assistant_messages = [
+            m for m in self.conversation_history if m.get("role") == "assistant"
+        ]
+        if not assistant_messages:
+            _cprint("  Nothing to copy yet — no assistant responses.")
+            return
+
+        # Search backward for the most recent message with code blocks
+        last = assistant_messages[-1].get("content", "")
+        if isinstance(last, list):
+            last = _assistant_content_as_text(last) if last else ""
+        elif last is None:
+            last = ""
+
+        fences = parse_code_fences(last)
+        closed_fences = [f for f in fences if f["closed"]]
+
+        if not closed_fences:
+            # Search earlier assistant messages
+            for msg in reversed(assistant_messages):
+                content = msg.get("content", "")
+                if isinstance(content, list):
+                    content = _assistant_content_as_text(content) if content else ""
+                elif content is None:
+                    content = ""
+                fences = parse_code_fences(content)
+                closed_fences = [f for f in fences if f["closed"]]
+                if closed_fences:
+                    last = content
+                    break
+
+        if not closed_fences:
+            _cprint("  No code blocks found in the last assistant response.")
+            return
+
+        # If a block index was supplied, copy directly.
+        if raw_arg:
+            try:
+                block_index = int(raw_arg) - 1  # 1-based → 0-based
+            except ValueError:
+                _cprint("  Usage: /copy-code [block_number]")
+                return
+            if block_index < 0 or block_index >= len(closed_fences):
+                _cprint(f"  Invalid block number. Use 1-{len(closed_fences)}.")
+                return
+            fence = closed_fences[block_index]
+            self._copy_fence(fence, block_index + 1)
+            return
+
+        # --- No argument: one-block immediate copy, multi-block numbered list ---
+        if len(closed_fences) == 1:
+            self._copy_fence(closed_fences[0], 1)
+            return
+
+        # Multiple blocks: show numbered preview list
+        self._code_block_preview(closed_fences)
+
+    def _copy_fence(self, fence: dict, block_num: int) -> None:
+        """Copy a single fence's raw content to clipboard with confirmation."""
+        from cli import _cprint
+
+        try:
+            self._write_osc52_clipboard(fence["raw_content"])
+            _cprint(
+                f"  Copied code block #{block_num} "
+                f"({fence['language']}) to clipboard"
+            )
+        except Exception as e:
+            _cprint(
+                f"  Failed to copy code block #{block_num} to clipboard: {e}"
+            )
+
+    def _code_block_preview(self, fences: list[dict]) -> None:
+        """Print numbered list of code blocks with language and first-line preview."""
+        from cli import _cprint, _DIM, _RST
+
+        lines: list[str] = []
+        for i, f in enumerate(fences, start=1):
+            raw = f["raw_content"]
+            # First line as preview
+            first_line = raw.split("\n")[0].strip() if raw else ""
+            if len(first_line) > 50:
+                first_line = first_line[:47] + "..."
+            lines.append(f"  {i}  {f['language']:12s} {first_line}")
+
+        _cprint("  Code blocks in the last response:")
+        for line in lines:
+            _cprint(line)
+        _cprint(f"\n  {_DIM}Run /cc <1-{len(fences)}> to copy a block{_RST}")
+

ui-tui/src/components/copyblox.tsx
@@ -1,0 +1,206 @@
+import { Box, NoSelect, stringWidth, Text } from '@hermes/ink'
+import type { ClickEvent } from '@hermes/ink'
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react'
+
+import { copyText } from '../lib/copyText.js'
+import type { Theme } from '../theme.js'
+
+// Copy state machine states
+type CopyState = 'idle' | 'copying' | 'copied' | 'failed'
+
+// 3-cell copy icon — visible enough to be clearly clickable.
+const COPY_ICON = '⧉⧉⧉'
+
+// Feedback labels shown in a compact single-row header.
+const FEEDBACK: Record<CopyState, string> = {
+  idle: '',
+  copying: '…',
+  copied: '✓',
+  failed: '!'
+}
+
+const COPY_FEEDBACK_MS = 1200
+const FAIL_FEEDBACK_MS = 1500
+
+interface CopyBloxProps {
+  children: React.ReactNode
+  closed: boolean
+  language: string
+  rawContent: string
+  theme: Theme
+  cols: number
+}
+
+export const CopyBlox = memo(function CopyBlox({ children, closed, language, rawContent, theme, cols }: CopyBloxProps) {
+  const [copyState, setCopyState] = useState<CopyState>('idle')
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+  const rawContentRef = useRef(rawContent)
+  const busyRef = useRef(false)
+
+  // Always keep ref current so click handler sees latest content
+  rawContentRef.current = rawContent
+
+  const doCopy = useCallback(async () => {
+    if (busyRef.current) {
+      return
+    }
+
+    busyRef.current = true
+
+    setCopyState('copying')
+
+    const text = rawContentRef.current
+    const result = await copyText(text)
+
+    if (result.success) {
+      setCopyState('copied')
+    } else {
+      setCopyState('failed')
+    }
+
+    const ms = result.success ? COPY_FEEDBACK_MS : FAIL_FEEDBACK_MS
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current)
+    }
+
+    const timer = setTimeout(() => {
+      setCopyState('idle')
+      busyRef.current = false
+    }, ms)
+
+    timerRef.current = timer
+  }, [])
+
+  // Clean up timer on unmount
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+    }
+  }, [])
+
+  const t = theme.color
+  const label = language || 'text'
+  const isStreaming = !closed
+
+  const w = (s: string) => stringWidth(s)
+  const fill = (n: number) => '─'.repeat(Math.max(1, n))
+
+  // ── Code body rows ─────────────────────────────────────────────────
+  const codeRows: React.ReactNode[] = []
+  let codeIdx = 0
+  React.Children.forEach(children, child => {
+    if (child == null) {
+      return
+    }
+
+    codeRows.push(
+      <Box key={codeIdx++}>
+        <Text color={t.border}>{'\u2502'}</Text>
+        {child}
+        <Text color={t.border}>{'\u2502'}</Text>
+      </Box>
+    )
+  })
+
+  // ── Bottom border ──────────────────────────────────────────────────
+  const bottomFillWidth = Math.max(1, cols - 2) // └ + ┘
+
+  // ── Header ─────────────────────────────────────────────────────────
+  // All headers are single-row: ┌─label─spacer─icon─┐
+  // The 3-cell icon (⧉⧉⧉) is wide enough to be clearly visible.
+
+  const labelW = w(label)
+  const iconW = 3 // 3 cells wide
+
+  if (isStreaming) {
+    // ── Single-row streaming header ──────────────────────────────────
+    const fixed = 1 + 1 + labelW + iconW + 1 // ┌─label─icon─┐
+    const spacerW = Math.max(0, cols - fixed)
+
+    return (
+      <Box flexDirection="column">
+        <NoSelect onClick={(e: ClickEvent) => { e.stopImmediatePropagation(); doCopy() }}>
+        <Box>
+          <Text color={t.border}>{'\u250c'}</Text>
+          <Text color={t.border}>{'\u2500'}</Text>
+          <Text color={t.accent}>{label}</Text>
+          {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+          <Text color={t.accent}>{'\u27f3'}</Text>
+          <Text color={t.border}>{'\u2510'}</Text>
+        </Box>
+        </NoSelect>
+
+        <Box flexDirection="column">{codeRows}</Box>
+
+        <Box>
+          <Text color={t.border}>{'\u2514'}</Text>
+          <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+          <Text color={t.border}>{'\u2518'}</Text>
+        </Box>
+      </Box>
+    )
+  }
+
+  if (copyState !== 'idle') {
+    // ── Single-row feedback header ───────────────────────────────────
+    const fixed = 1 + 1 + labelW + 4 + 1 // ┌─label─" … "─┐
+    const spacerW = Math.max(0, cols - fixed)
+
+    return (
+      <Box flexDirection="column">
+        <NoSelect onClick={(e: ClickEvent) => { e.stopImmediatePropagation(); doCopy() }}>
+        <Box>
+          <Text color={t.border}>{'\u250c'}</Text>
+          <Text color={t.border}>{'\u2500'}</Text>
+          <Text color={t.accent}>{label}</Text>
+          {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+          <Text color={t.border}>{' '}</Text>
+          <Text color={t.accent}>{FEEDBACK[copyState]}</Text>
+          <Text color={t.border}>{' '}</Text>
+          <Text color={t.border}>{'\u2510'}</Text>
+        </Box>
+        </NoSelect>
+
+        <Box flexDirection="column">{codeRows}</Box>
+
+        <Box>
+          <Text color={t.border}>{'\u2514'}</Text>
+          <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+          <Text color={t.border}>{'\u2518'}</Text>
+        </Box>
+      </Box>
+    )
+  }
+
+  // ── Single-row idle header with copy icon ──────────────────────────
+  const fixed = 1 + 1 + labelW + iconW + 1 // ┌─label─icon─┐
+  const spacerW = Math.max(0, cols - fixed)
+
+  return (
+    <Box flexDirection="column">
+      <NoSelect onClick={(e: ClickEvent) => { e.stopImmediatePropagation(); doCopy() }}>
+      <Box>
+        <Text color={t.border}>{'\u250c'}</Text>
+        <Text color={t.border}>{'\u2500'}</Text>
+        <Text color={t.accent}>{label}</Text>
+        {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+        <Text color={t.accent}>{COPY_ICON}</Text>
+        <Text color={t.border}>{'\u2510'}</Text>
+      </Box>
+      </NoSelect>
+
+      {/* Code body with side borders — not inside click target */}
+      <Box flexDirection="column">{codeRows}</Box>
+
+      {/* Bottom border */}
+      <Box>
+        <Text color={t.border}>{'\u2514'}</Text>
+        <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+        <Text color={t.border}>{'\u2518'}</Text>
+      </Box>
+    </Box>
+  )
+})

NousResearch/hermes-agent

hermes_cli/cli_commands_mixin.py
@@ -3213,6 +3213,84 @@ class CLICommandsMixin:
             _cprint(f"Unknown voice subcommand: {subcommand}")
             _cprint("Usage: /voice [on|off|tts|status]")
 
+    def _handle_copy_code_command(self, cmd_original: str) -> None:
+        """Handle /copy-code [block_number] for the latest fenced code block."""
+        from cli import _assistant_content_as_text, _cprint
+        from hermes_cli.code_fences import parse_code_fences
+
+        parts = cmd_original.split(maxsplit=1)
+        raw_arg = parts[1].strip() if len(parts) > 1 else ""
+        if raw_arg and (not raw_arg.isdecimal() or int(raw_arg) < 1):
+            _cprint("  Usage: /copy-code [block_number]")
+            return
+
+        assistant_messages = [
+            message for message in self.conversation_history
+            if message.get("role") == "assistant"
+        ]
+        if not assistant_messages:
+            _cprint("  Nothing to copy yet — no assistant responses.")
+            return
+
+        closed_fences: list[dict] = []
+        for message in reversed(assistant_messages):
+            content = message.get("content", "")
+            if isinstance(content, list):
+                content = _assistant_content_as_text(content) if content else ""
+            elif content is None:
+                content = ""
+            closed_fences = [fence for fence in parse_code_fences(content) if fence["closed"]]
+            if closed_fences:
+                break
+
+        if not closed_fences:
+            _cprint("  No complete code blocks found in assistant responses.")
+            return
+
+        if raw_arg:
+            block_index = int(raw_arg) - 1
+            if block_index >= len(closed_fences):
+                _cprint(f"  Invalid block number. Use 1-{len(closed_fences)}.")
+                return
+            self._copy_fence(closed_fences[block_index], block_index + 1)
+            return
+
+        if len(closed_fences) == 1:
+            self._copy_fence(closed_fences[0], 1)
+            return
+
+        self._code_block_preview(closed_fences)
+
+    def _copy_fence(self, fence: dict, block_num: int) -> None:
+        """Copy raw fenced content using the same policy as /copy."""
+        from cli import _cprint
+        from hermes_cli.clipboard import is_remote_shell_session, write_clipboard_text
+
+        text = fence["raw_content"]
+        try:
+            if is_remote_shell_session():
+                self._write_osc52_clipboard(text)
+                method = " via OSC 52 (terminal support required)"
+            elif write_clipboard_text(text):
+                method = ""
+            else:
+                self._write_osc52_clipboard(text)
+                method = " via OSC 52 (terminal support required)"
+            _cprint(f"  Copied code block #{block_num} ({fence['language'] or 'text'}) to clipboard{method}")
+        except Exception as exc:
+            _cprint(f"  Failed to copy code block #{block_num} to clipboard: {exc}")
+
+    def _code_block_preview(self, fences: list[dict]) -> None:
+        """Print a numbered list of fences for a follow-up /cc selection."""
+        from cli import _DIM, _RST, _cprint
+
+        _cprint("  Code blocks in the last response:")
+        for index, fence in enumerate(fences, start=1):
+            first_line = fence["raw_content"].split("\n", 1)[0].strip()
+            preview = first_line[:47] + "..." if len(first_line) > 50 else first_line
+            _cprint(f"  {index}  {fence['language'] or 'text':12s} {preview}")
+        _cprint(f"\n  {_DIM}Run /cc <1-{len(fences)}> to copy a block{_RST}")
+
     def _handle_wake_command(self, command: str):
         """Handle /wake [on|off|status] — the 'Hey Hermes' hotword listener.
 

ui-tui/src/components/copyblox.tsx
@@ -1,0 +1,206 @@
+import { Box, NoSelect, stringWidth, Text } from '@hermes/ink'
+import type { ClickEvent } from '@hermes/ink'
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react'
+
+import { copyText } from '../lib/copyText.js'
+import type { Theme } from '../theme.js'
+
+// Copy state machine states
+type CopyState = 'idle' | 'copying' | 'copied' | 'failed'
+
+// 3-cell copy icon — visible enough to be clearly clickable.
+const COPY_ICON = '⧉⧉⧉'
+
+// Feedback labels shown in a compact single-row header.
+const FEEDBACK: Record<CopyState, string> = {
+  idle: '',
+  copying: '…',
+  copied: '✓',
+  failed: '!'
+}
+
+const COPY_FEEDBACK_MS = 1200
+const FAIL_FEEDBACK_MS = 1500
+
+interface CopyBloxProps {
+  children: React.ReactNode
+  closed: boolean
+  language: string
+  rawContent: string
+  theme: Theme
+  cols: number
+}
+
+export const CopyBlox = memo(function CopyBlox({ children, closed, language, rawContent, theme, cols }: CopyBloxProps) {
+  const [copyState, setCopyState] = useState<CopyState>('idle')
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+  const rawContentRef = useRef(rawContent)
+  const busyRef = useRef(false)
+
+  // Always keep ref current so click handler sees latest content
+  rawContentRef.current = rawContent
+
+  const doCopy = useCallback(async () => {
+    if (busyRef.current) {
+      return
+    }
+
+    busyRef.current = true
+
+    setCopyState('copying')
+
+    const text = rawContentRef.current
+    const result = await copyText(text)
+
+    if (result.success) {
+      setCopyState('copied')
+    } else {
+      setCopyState('failed')
+    }
+
+    const ms = result.success ? COPY_FEEDBACK_MS : FAIL_FEEDBACK_MS
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current)
+    }
+
+    const timer = setTimeout(() => {
+      setCopyState('idle')
+      busyRef.current = false
+    }, ms)
+
+    timerRef.current = timer
+  }, [])
+
+  // Clean up timer on unmount
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+    }
+  }, [])
+
+  const t = theme.color
+  const label = language || 'text'
+  const isStreaming = !closed
+
+  const w = (s: string) => stringWidth(s)
+  const fill = (n: number) => '─'.repeat(Math.max(1, n))
+
+  // ── Code body rows ─────────────────────────────────────────────────
+  const codeRows: React.ReactNode[] = []
+  let codeIdx = 0
+  React.Children.forEach(children, child => {
+    if (child == null) {
+      return
+    }
+
+    codeRows.push(
+      <Box key={codeIdx++}>
+        <Text color={t.border}>{'\u2502'}</Text>
+        {child}
+        <Text color={t.border}>{'\u2502'}</Text>
+      </Box>
+    )
+  })
+
+  // ── Bottom border ──────────────────────────────────────────────────
+  const bottomFillWidth = Math.max(1, cols - 2) // └ + ┘
+
+  // ── Header ─────────────────────────────────────────────────────────
+  // All headers are single-row: ┌─label─spacer─icon─┐
+  // The 3-cell icon (⧉⧉⧉) is wide enough to be clearly visible.
+
+  const labelW = w(label)
+  const iconW = 3 // 3 cells wide
+
+  if (isStreaming) {
+    // ── Single-row streaming header ──────────────────────────────────
+    const fixed = 1 + 1 + labelW + iconW + 1 // ┌─label─icon─┐
+    const spacerW = Math.max(0, cols - fixed)
+
+    return (
+      <Box flexDirection="column">
+        <NoSelect>
+        <Box>
+          <Text color={t.border}>{'\u250c'}</Text>
+          <Text color={t.border}>{'\u2500'}</Text>
+          <Text color={t.accent}>{label}</Text>
+          {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+          <Text color={t.muted}>{'\u27f3'}</Text>
+          <Text color={t.border}>{'\u2510'}</Text>
+        </Box>
+        </NoSelect>
+
+        <Box flexDirection="column">{codeRows}</Box>
+
+        <Box>
+          <Text color={t.border}>{'\u2514'}</Text>
+          <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+          <Text color={t.border}>{'\u2518'}</Text>
+        </Box>
+      </Box>
+    )
+  }
+
+  if (copyState !== 'idle') {
+    // ── Single-row feedback header ───────────────────────────────────
+    const fixed = 1 + 1 + labelW + 4 + 1 // ┌─label─" … "─┐
+    const spacerW = Math.max(0, cols - fixed)
+
+    return (
+      <Box flexDirection="column">
+        <NoSelect onClick={(e: ClickEvent) => { e.stopImmediatePropagation(); doCopy() }}>
+        <Box>
+          <Text color={t.border}>{'\u250c'}</Text>
+          <Text color={t.border}>{'\u2500'}</Text>
+          <Text color={t.accent}>{label}</Text>
+          {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+          <Text color={t.border}>{' '}</Text>
+          <Text color={t.accent}>{FEEDBACK[copyState]}</Text>
+          <Text color={t.border}>{' '}</Text>
+          <Text color={t.border}>{'\u2510'}</Text>
+        </Box>
+        </NoSelect>
+
+        <Box flexDirection="column">{codeRows}</Box>
+
+        <Box>
+          <Text color={t.border}>{'\u2514'}</Text>
+          <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+          <Text color={t.border}>{'\u2518'}</Text>
+        </Box>
+      </Box>
+    )
+  }
+
+  // ── Single-row idle header with copy icon ──────────────────────────
+  const fixed = 1 + 1 + labelW + iconW + 1 // ┌─label─icon─┐
+  const spacerW = Math.max(0, cols - fixed)
+
+  return (
+    <Box flexDirection="column">
+      <NoSelect onClick={(e: ClickEvent) => { e.stopImmediatePropagation(); doCopy() }}>
+      <Box>
+        <Text color={t.border}>{'\u250c'}</Text>
+        <Text color={t.border}>{'\u2500'}</Text>
+        <Text color={t.accent}>{label}</Text>
+        {spacerW > 0 ? <Text color={t.border}>{fill(spacerW)}</Text> : null}
+        <Text color={t.accent}>{COPY_ICON}</Text>
+        <Text color={t.border}>{'\u2510'}</Text>
+      </Box>
+      </NoSelect>
+
+      {/* Code body with side borders — not inside click target */}
+      <Box flexDirection="column">{codeRows}</Box>
+
+      {/* Bottom border */}
+      <Box>
+        <Text color={t.border}>{'\u2514'}</Text>
+        <Text color={t.border}>{fill(bottomFillWidth)}</Text>
+        <Text color={t.border}>{'\u2518'}</Text>
+      </Box>
+    </Box>
+  )
+})
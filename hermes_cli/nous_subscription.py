bf3e3699a684ab2a8d993491212b6f23b7c8bf65
Seppe Gadeyne <seppe@fushia.be>
2026-07-31 12:42:38 +0200

    fix(web): preserve gateway precedence for SearXNG

--- hermes_cli/nous_subscription.py
+++ hermes_cli/nous_subscription.py
@@ -460,6 +460,7 @@ def get_nous_subscription_features(
         direct_exa = False
         direct_parallel = False
         direct_tavily = False
+        direct_searxng = False
         direct_brave = False
     if image_use_gateway:
         direct_fal = False

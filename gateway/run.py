ai-integr8tor/NousResearch-hermes-agent

fix(gateway): route restart internals away from shared chats
Restart lifecycle replies are deterministic gateway-internal output, but the
adapter sinks treated them as ordinary text and defaulted them back to the
origin chat. When /restart was triggered from a group, the immediate command
reply and post-restart notification could therefore expose operational status
to the shared audience.

Add a PrivateReply wrapper and adapter sink helper that prefer a real
send_private_notice implementation for shared-audience sources, while refusing
to reuse the default public fallback for confidential text. /restart now
persists the requester user id for the restarted process, marks restart status
as private, and sends only a neutral public fallback when private delivery is
not available.

Related #48060
Related #24365
Co-authored-by: Aldo <17973757+aldoeliacim@users.noreply.github.com>
Co-authored-by: Teknium <127238744+teknium1@users.noreply.github.com>

gateway/run.py
@@ -1675,6 +1675,7 @@ from gateway.session import (
     build_session_context,
     build_session_context_prompt,
     build_session_key,
+    is_shared_audience,
     is_shared_multi_user_session,
 )
 from gateway.delivery import DeliveryRouter, looks_like_telegram_private_chat_id
@@ -14156,6 +14157,8 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
             platform_str = data.get("platform")
             chat_id = data.get("chat_id")
             chat_type = data.get("chat_type")
+            user_id = data.get("user_id")
+            user_name = data.get("user_name")
             thread_id = data.get("thread_id")
             message_id = data.get("message_id")
 
@@ -14187,11 +14190,50 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                 reply_to_message_id=message_id,
                 adapter=adapter,
             )
-            result = await adapter.send(
-                str(chat_id),
-                "♻ Gateway restarted successfully. Your session continues.",
-                metadata=_non_conversational_metadata(metadata, platform=platform),
+            source = SessionSource(
+                platform=platform,
+                chat_id=str(chat_id),
+                chat_type=chat_type or "dm",
+                user_id=str(user_id) if user_id else None,
+                user_name=str(user_name) if user_name else None,
+                thread_id=str(thread_id) if thread_id else None,
+                message_id=str(message_id) if message_id else None,
             )
+            private_text = "♻ Gateway restarted successfully. Your session continues."
+            metadata = _non_conversational_metadata(metadata, platform=platform)
+            result = None
+            if is_shared_audience(source):
+                if (
+                    source.user_id
+                    and getattr(adapter, "_supports_private_notice_delivery", lambda: False)()
+                ):
+                    result = await adapter.send_private_notice(
+                        str(chat_id),
+                        source.user_id,
+                        private_text,
+                        metadata=metadata,
+                    )
+                    if result is not None and getattr(result, "success", True) is False:
+                        logger.warning(
+                            "Private restart notification to %s:%s user %s was not delivered: %s",
+                            platform_str,
+                            chat_id,
+                            source.user_id,
+                            getattr(result, "error", "send returned success=False"),
+                        )
+                        result = None
+                if result is None:
+                    result = await adapter.send(
+                        str(chat_id),
+                        "♻ Gateway restarted successfully. Operational details were kept private.",
+                        metadata=metadata,
+                    )
+            else:
+                result = await adapter.send(
+                    str(chat_id),
+                    private_text,
+                    metadata=metadata,
+                )
             # adapter.send() catches provider errors (e.g. "Chat not found")
             # and returns SendResult(success=False) rather than raising, so
             # we must inspect the result before claiming success — otherwise

NousResearch/hermes-agent

gateway/run.py
@@ -2214,6 +2214,7 @@ from gateway.session import (
     build_session_context_prompt,
     build_channel_continuity_note,
     build_session_key,
+    is_shared_audience,
     is_shared_multi_user_session,
     neutralize_untrusted_inline_text,
 )
@@ -20202,6 +20203,8 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
             platform_str = data.get("platform")
             chat_id = data.get("chat_id")
             chat_type = data.get("chat_type")
+            user_id = data.get("user_id")
+            user_name = data.get("user_name")
             thread_id = data.get("thread_id")
             message_id = data.get("message_id")
 
@@ -20239,12 +20242,60 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                     metadata["user_id"] = str(data["user_id"])
                 if data.get("scope_id"):
                     metadata["scope_id"] = str(data["scope_id"])
-            result = await transport.send(
-                platform,
-                str(chat_id),
-                "♻ Gateway restarted successfully. Your session continues.",
-                metadata=_non_conversational_metadata(metadata, platform=platform),
+            source = SessionSource(
+                platform=platform,
+                chat_id=str(chat_id),
+                chat_type=chat_type or "dm",
+                user_id=str(user_id) if user_id else None,
+                user_name=str(user_name) if user_name else None,
+                thread_id=str(thread_id) if thread_id else None,
+                message_id=str(message_id) if message_id else None,
+                scope_id=str(data["scope_id"]) if data.get("scope_id") else None,
+                delivered_via_upstream_relay=(
+                    data.get("delivered_via_upstream_relay") is True
+                ),
             )
+            private_text = "♻ Gateway restarted successfully. Your session continues."
+            metadata = _non_conversational_metadata(metadata, platform=platform)
+            result = None
+            if is_shared_audience(source):
+                if (
+                    source.user_id
+                    and getattr(
+                        transport.adapter,
+                        "_supports_private_notice_delivery",
+                        lambda: False,
+                    )()
+                ):
+                    result = await transport.adapter.send_private_notice(
+                        str(chat_id),
+                        source.user_id,
+                        private_text,
+                        metadata=metadata,
+                    )
+                    if result is not None and getattr(result, "success", True) is False:
+                        logger.warning(
+                            "Private restart notification to %s:%s user %s was not delivered: %s",
+                            platform_str,
+                            chat_id,
+                            source.user_id,
+                            getattr(result, "error", "send returned success=False"),
+                        )
+                        result = None
+                if result is None:
+                    result = await transport.send(
+                        platform,
+                        str(chat_id),
+                        "♻ Gateway restarted successfully. Operational details were kept private.",
+                        metadata=metadata,
+                    )
+            else:
+                result = await transport.send(
+                    platform,
+                    str(chat_id),
+                    private_text,
+                    metadata=metadata,
+                )
             # adapter.send() catches provider errors (e.g. "Chat not found")
             # and returns SendResult(success=False) rather than raising, so
             # we must inspect the result before claiming success — otherwise
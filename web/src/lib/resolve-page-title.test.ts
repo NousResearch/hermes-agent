bottombgr-design/hermes

feat(dashboard): support root dashboard landing pages

web/src/lib/resolve-page-title.test.ts
@@ -1,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import { en } from "@/i18n/en";
+import { resolvePageTitle } from "./resolve-page-title";
+
+describe("resolvePageTitle", () => {
+  it("uses root dashboard plugin metadata before the Sessions fallback", () => {
+    expect(
+      resolvePageTitle("/", en, [{ path: "/", label: "Command Center" }]),
+    ).toBe("Command Center");
+  });
+
+  it("keeps the historical Sessions fallback without a root plugin", () => {
+    expect(resolvePageTitle("/", en, [])).toBe(en.app.nav.sessions);
+  });
+});

NousResearch/hermes-agent

web/src/lib/resolve-page-title.test.ts
@@ -49,7 +49,13 @@ describe("resolvePageTitle", () => {
     expect(resolvePageTitle("/whatever", t, [])).toBe("Whatever");
   });
 
-  it("treats root as sessions and trailing slashes as equivalent", () => {
+  it("uses root dashboard plugin metadata before the Sessions fallback", () => {
+    expect(
+      resolvePageTitle("/", t, [{ path: "/", label: "Command Center" }]),
+    ).toBe("Command Center");
+  });
+
+  it("treats root as sessions without a root plugin and trailing slashes as equivalent", () => {
     expect(resolvePageTitle("/", t, [])).toBe("Sessions");
     expect(resolvePageTitle("/mcp/", t, [])).toBe("MCP");
   });
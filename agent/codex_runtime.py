Carrie111998/Hermes

fix(codex): emit reasoning from completed Responses items
Codex Responses backends can send a turn's entire reasoning summary only on
`response.output_item.done`, with no `reasoning*.delta` frames. Hermes consumed
only deltas, so the model had reasoned but the user saw an empty thinking block.

Backfill `on_reasoning_delta` from completed `type="reasoning"` items. The
backfill is per-item: collect deltas for the currently open reasoning item,
then compare the completed summary at its done event:

- no deltas -> emit the whole summary;
- completed summary extends the streamed prefix -> emit only the missing tail;
- completed summary is a rewrite with no streamed-prefix match -> emit it all;
- completed summary is already contained in streamed text -> emit nothing.

Reset the delta baseline when each reasoning item closes. A reason -> tool_call
-> reason turn has multiple reasoning items; a turn-wide buffer would compare
the second item's summary to the first item's deltas and either duplicate it or
suppress it as a false duplicate.

The completed-item extractor accepts both SDK attr objects and dict-shaped
items through the existing `_item_field` helper. It uses summary parts first,
then falls back to a top-level `text` field used by compatible Responses
backends. Multiple summary parts join with two newlines, matching the existing
`summary_index` delta boundary.

Tests cover: summary-only item; already-streamed summary (no duplicate);
streamed prefix plus completed tail; two reasoning items with baseline reset;
and a non-reasoning completed item that must never fire the reasoning callback.

agent/codex_runtime.py
@@ -1017,6 +1017,48 @@ def _item_field(item: Any, name: str, default: Any = None) -> Any:
     return value if value is not None else default
 
 
+def _completed_reasoning_text(item: Any) -> str:
+    """Extract displayable text from a completed Responses ``reasoning`` item.
+
+    Some backends deliver a turn's reasoning ONLY on
+    ``response.output_item.done`` — no ``reasoning*.delta`` frames at all — so a
+    consumer that reads deltas alone shows an empty thinking block for a turn
+    that did reason. Joining the summary parts with ``\n\n`` mirrors how the
+    delta path separates them via ``summary_index``.
+    """
+    if _item_field(item, "type", "") != "reasoning":
+        return ""
+
+    summary = _item_field(item, "summary")
+    if isinstance(summary, list):
+        parts = []
+        for part in summary:
+            text = _item_field(part, "text", "")
+            if isinstance(text, str) and text:
+                parts.append(text)
+        if parts:
+            return "\n\n".join(parts).strip()
+
+    text = _item_field(item, "text", "")
+    return text.strip() if isinstance(text, str) else ""
+
+
+def _missing_reasoning_tail(completed: str, streamed: str) -> str:
+    """Return the part of ``completed`` not already delivered as deltas.
+
+    Four cases, in order: nothing streamed (send it all), the summary extends
+    what streamed (send only the tail), the summary is a rewrite that shares no
+    prefix (send it all), or it was already fully delivered (send nothing).
+    """
+    if not streamed:
+        return completed
+    if completed.startswith(streamed):
+        return completed[len(streamed):]
+    if completed not in streamed:
+        return completed
+    return ""
+
+
 def _raise_stream_error(event: Any) -> None:
     """Raise a ``_StreamErrorEvent`` from a ``type=error`` SSE frame.
 
@@ -1127,6 +1169,12 @@ def _consume_codex_event_stream(
     # parts by this index and gives each part no separator of its own, so a
     # change of index is where the blank line belongs.
     active_summary_index: Any = None
+    # Reasoning deltas delivered for the CURRENTLY OPEN reasoning item, so a
+    # completed summary can be diffed against what the user already saw. Reset
+    # when a reasoning item closes: a reason -> tool_call -> reason turn emits
+    # several items, and comparing the second summary against the first item's
+    # deltas would either re-emit it whole or suppress it as a false duplicate.
+    current_reasoning_deltas: list[str] = []
     terminal_status: str = "completed"
     terminal_usage: Any = None
     terminal_response_id: str = None
@@ -1280,6 +1328,7 @@ def _consume_codex_event_stream(
                     reasoning_text = f"\n\n{reasoning_text}"
                 if summary_index is not None:
                     active_summary_index = summary_index
+                current_reasoning_deltas.append(reasoning_text)
                 try:
                     on_reasoning_delta(reasoning_text)
                 except Exception:
@@ -1290,6 +1339,22 @@ def _consume_codex_event_stream(
             done_item = _event_field(event, "item")
             if done_item is not None:
                 collected_output_items.append(done_item)
+                completed_reasoning = _completed_reasoning_text(done_item)
+                if completed_reasoning and on_reasoning_delta is not None:
+                    missing_reasoning = _missing_reasoning_tail(
+                        completed_reasoning,
+                        "".join(current_reasoning_deltas).strip(),
+                    )
+                    if missing_reasoning:
+                        try:
+                            on_reasoning_delta(missing_reasoning)
+                        except Exception:
+                            logger.debug(
+                                "Codex completed reasoning callback raised",
+                                exc_info=True,
+                            )
+                if _item_field(done_item, "type", "") == "reasoning":
+                    current_reasoning_deltas = []
                 # Reuse the first-observed position when this item was
                 # announced earlier via output_item.added; a fresh tail
                 # sequence is allocated only for genuinely unannounced items.

NousResearch/hermes-agent

agent/codex_runtime.py
@@ -544,6 +544,34 @@ def _codex_event_has_content(event: Any) -> bool:
     return False
 
 
+def _completed_reasoning_text(item: Any) -> str:
+    """Extract displayable text from a completed Responses reasoning item."""
+    if _event_field(item, "type", "") != "reasoning":
+        return ""
+    summary = _event_field(item, "summary")
+    if isinstance(summary, list):
+        parts = [
+            text
+            for part in summary
+            if isinstance((text := _event_field(part, "text", "")), str) and text
+        ]
+        if parts:
+            return "\n\n".join(parts).strip()
+    text = _event_field(item, "text", "")
+    return text.strip() if isinstance(text, str) else ""
+
+
+def _missing_reasoning_tail(completed: str, streamed: str) -> str:
+    """Return completed reasoning not already delivered as deltas."""
+    if not streamed:
+        return completed
+    if completed.startswith(streamed):
+        return completed[len(streamed):]
+    if completed not in streamed:
+        return completed
+    return ""
+
+
 def _raise_stream_error(event: Any) -> None:
     """Raise ``_StreamErrorEvent`` from a ``type=error`` SSE frame. The spec puts code/message/param at the
     top level, but the SDK and several proxies nest them under ``error``; read top-level first, then the envelope."""
@@ -596,7 +624,7 @@ class _CodexResponseAssembler:
         # output_index / first-observed sequence per output item, in lockstep, so settled pending calls merge
         # back in stream order.
         self.output_indexes, self.output_sequences = [], []
-        self.text_deltas, self.commentary_text_deltas = [], []
+        self.text_deltas, self.commentary_text_deltas, self.current_reasoning_deltas = [], [], []
         # pending_function_calls: announced-but-unconfirmed function calls keyed by item id. announced_output_order:
         # first-observed (sequence, output_index) per announced item id so a later .done keeps its announced position.
         self.pending_function_calls: Dict[str, Dict[str, Any]] = {}
@@ -670,6 +698,7 @@ class _CodexResponseAssembler:
             if self.active_summary_index is not None and summary_index != self.active_summary_index:
                 reasoning_text = f"\n\n{reasoning_text}"
             self.active_summary_index = summary_index
+        self.current_reasoning_deltas.append(reasoning_text)
         self._safe(self.on_reasoning_delta, "on_reasoning_delta", reasoning_text)
 
     def _on_item_done(self, event: Any, event_type: str) -> None:
@@ -677,6 +706,14 @@ class _CodexResponseAssembler:
         if done_item is None:
             return
         self.output_items.append(done_item)
+        completed_reasoning = _completed_reasoning_text(done_item)
+        if completed_reasoning and self.on_reasoning_delta is not None:
+            missing_reasoning = _missing_reasoning_tail(
+                completed_reasoning, "".join(self.current_reasoning_deltas).strip())
+            if missing_reasoning:
+                self._safe(self.on_reasoning_delta, "completed reasoning callback", missing_reasoning)
+        if _event_field(done_item, "type", "") == "reasoning":
+            self.current_reasoning_deltas = []
         # Reuse the announced position when known (fresh tail sequence for unannounced items); the .done
         # event's own output_index wins over the announced one.
         done_id = str(_event_field(done_item, "id", ""))
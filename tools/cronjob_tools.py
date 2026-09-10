Carrie111998/Hermes

feat(cron): expose monitor commit policy through job APIs
Make after_delivery usable without hand-editing jobs.json:

- validate and persist monitor_commit_policy in create_job/update_job
- expose the field through the cronjob model schema and handler
- add hermes cron create/edit flags and list output
- cover core API, model-tool, and CLI round trips

The eager detection_time behavior remains the default when the field is
absent, preserving existing job records and runtime semantics.

tools/cronjob_tools.py
@@ -785,6 +785,8 @@ def _format_job(job: Dict[str, Any]) -> Dict[str, Any]:
         result["monitor_script"] = job["monitor_script"]
     if job.get("monitor_url"):
         result["monitor_url"] = job["monitor_url"]
+    if job.get("monitor_commit_policy"):
+        result["monitor_commit_policy"] = job["monitor_commit_policy"]
     if job.get("monitor_state"):
         result["monitor_state"] = job["monitor_state"]
     if job.get("no_agent"):
@@ -1482,6 +1484,7 @@ def cronjob(
     monitor_script: Optional[str] = None,
     monitor_url: Optional[str] = None,
     reasoning_effort: Optional[str] = None,
+    monitor_commit_policy: Optional[str] = None,
     task_id: str = None,
     session_id: Optional[str] = None,
 ) -> str:
@@ -1592,6 +1595,7 @@ def cronjob(
                     attach_to_session=attach_to_session,
                     monitor_script=_normalize_optional_job_value(monitor_script),
                     monitor_url=_normalize_optional_job_value(monitor_url),
+                    monitor_commit_policy=monitor_commit_policy,
                     # reasoning_effort reaches here from the CLI
                     # (hermes cron create --reasoning-effort) ONLY — it is
                     # deliberately absent from CRONJOB_SCHEMA and the model
@@ -1867,6 +1871,8 @@ def cronjob(
                         "clear one before setting the other.",
                         success=False,
                     )
+            if monitor_commit_policy is not None:
+                updates["monitor_commit_policy"] = monitor_commit_policy
             if context_from is not None or continuity is not None:
                 # Empty string / empty list clears the field; otherwise validate
                 # each referenced job exists before storing. Normalized to a list
@@ -1997,6 +2003,11 @@ Jobs run in a fresh session with no current-chat context, so prompts must be sel
                 "type": "string",
                 "description": "Optional change-detector that gates the agent: an http(s) URL (fetched each tick) or a script path (same rules as `script`, run each tick) — cheap, no LLM. Output identical to the previous tick skips the agent run entirely; changed output wakes the agent with a diff injected into the prompt. First tick always runs (baseline). Output must be deterministic (no timestamps) or every tick looks changed. Incompatible with no_agent. On update, '' clears."
             },
+            "monitor_commit_policy": {
+                "type": "string",
+                "enum": ["detection_time", "after_delivery"],
+                "description": "Optional commit boundary for monitor state. detection_time preserves the eager default. after_delivery leaves the prior hash intact until the triggered agent run and required delivery succeed, so transient failures retry the same change next tick. Requires monitor."
+            },
             "no_agent": {
                 "type": "boolean",
                 "default": False,
@@ -2092,6 +2103,7 @@ def _cronjob_handler(args, **kw):
         attach_to_session=args.get("attach_to_session"),
         monitor_script=_mon_script,
         monitor_url=_mon_url,
+        monitor_commit_policy=args.get("monitor_commit_policy"),
         task_id=kw.get("task_id"),
         session_id=kw.get("session_id"),
     )

NousResearch/hermes-agent

tools/cronjob_tools.py
@@ -572,6 +572,7 @@ def _action_create(a: Dict[str, Any]) -> str:
             no_agent=_no_agent, attach_to_session=a["attach_to_session"],
             monitor_script=_normalize_optional_job_value(a["monitor_script"]),
             monitor_url=_normalize_optional_job_value(a["monitor_url"]),
+            monitor_commit_policy=a["monitor_commit_policy"],
             # CLI-only lane: absent from CRONJOB_SCHEMA and the model dispatch (models don't pick models).
             reasoning_effort=a["reasoning_effort"],
             failure_deliver=_resolve_cron_context_deliver(_normalize_deliver_param(a["failure_deliver"])),
@@ -747,6 +748,9 @@ def _update_script_fields(job: Dict[str, Any], a: Dict[str, Any], updates: Dict[
     if (monitor_script is not None or monitor_url is not None) and (
         _pick(updates, job, "monitor_script") and _pick(updates, job, "monitor_url")):
         return("monitor_script and monitor_url are mutually exclusive — clear one before setting the other.")
+    if a["monitor_commit_policy"] is not None:
+        # update_job validates the value and drops it when no monitor source survives the merge.
+        updates["monitor_commit_policy"] = a["monitor_commit_policy"]
     return None
 
 
@@ -875,6 +879,7 @@ def cronjob(
     attach_to_session: Optional[bool] = None,
     monitor_script: Optional[str] = None,
     monitor_url: Optional[str] = None,
+    monitor_commit_policy: Optional[str] = None,
     reasoning_effort: Optional[str] = None,
     failure_deliver: Optional[Union[str, List[str]]] = None,
     task_id: str = None,
@@ -960,6 +965,11 @@ Jobs run in a fresh session with no current-chat context, so prompts must be sel
                 "type": "string",
                 "description": "Optional change-detector that gates the agent: an http(s) URL (fetched each tick) or a script path (same rules as `script`, run each tick) — cheap, no LLM. Output identical to the previous tick skips the agent run entirely; changed output wakes the agent with a diff injected into the prompt. First tick always runs (baseline). Output must be deterministic (no timestamps) or every tick looks changed. Incompatible with no_agent. On update, '' clears."
             },
+            "monitor_commit_policy": {
+                "type": "string",
+                "enum": ["detection_time", "after_delivery"],
+                "description": "Optional commit boundary for monitor state. detection_time (default) advances the stored hash as soon as the change is detected. after_delivery keeps the prior hash until the triggered agent run AND its delivery succeed, so a transient failure retries the same change next tick instead of losing it. Requires a monitor."
+            },
             "no_agent": {
                 "type": "boolean",
                 "default": False,
@@ -1011,7 +1021,7 @@ def check_cronjob_requirements() -> bool:
 _HANDLER_FORWARDED_ARGS = (
     "job_id", "prompt", "schedule", "name", "repeat", "deliver", "failure_deliver", "skill", "skills", "reason",
     "script", "context_from", "continuity", "enabled_toolsets", "workdir", "no_agent", "attach_to_session",
-    "paused_reason")
+    "monitor_commit_policy", "paused_reason")
 
 
 def _cronjob_handler(args, **kw):
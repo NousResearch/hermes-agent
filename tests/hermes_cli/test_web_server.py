Carrie111998/Hermes

fix(dashboard): let the inner TUI own chat transcript scrolling (#85366)
The dashboard PTY forced HERMES_TUI_INLINE=1 while xterm.js tried to scroll
an empty host scrollback buffer. Inline mode leaves the transcript ScrollBox
unbounded, so PageUp/wheel had nothing to scroll and only the last few rows
were visible.

Stop forcing inline mode so AlternateScreen bounds the viewport, route browser
wheel into Shift+Up/Down key sequences over the PTY, and keep burst collapse
in the resume sanitizer without stripping erase codes.

tests/hermes_cli/test_web_server.py
@@ -3779,7 +3779,26 @@ class TestPtyWebSocket:
         q = {"token": tok, **params}
         return f"/api/pty?{urlencode(q)}"
 
+    def test_resolve_chat_argv_uses_alt_screen_for_transcript_scroll(
+        self, monkeypatch,
+    ):
+        """Dashboard chat must not force inline mode — ScrollBox needs a bounded viewport."""
+        import hermes_cli.main as main_mod
 
+        monkeypatch.setattr(
+            main_mod,
+            "_make_tui_argv",
+            lambda project_root, tui_dev=False: (
+                ["node", "dist/entry.js"],
+                "/tmp/ui-tui",
+            ),
+        )
+
+        _argv, _cwd, env = self.ws_module._resolve_chat_argv()
+
+        assert "HERMES_TUI_INLINE" not in env
+        assert env["HERMES_TUI_DISABLE_MOUSE"] == "1"
+        assert env["HERMES_TUI_DASHBOARD"] == "1"
 
     def test_tui_python_command_uses_child_path(self, tmp_path):
         """Bare Python commands are resolved from the TUI child's PATH."""
@@ -3801,10 +3820,6 @@ class TestPtyWebSocket:
         main_mod._apply_tui_python_env(env)
 
         assert env["HERMES_PYTHON"] == command
-
-
-
-
 
     def test_resolve_chat_argv_async_uses_worker_thread(self, monkeypatch):
         captured: dict = {}

NousResearch/hermes-agent

tests/hermes_cli/test_web_server.py
@@ -4584,7 +4584,27 @@ class TestPtyWebSocket:
         q = {"token": tok, **params}
         return f"/api/pty?{urlencode(q)}"
 
+    def test_resolve_chat_argv_uses_alt_screen_for_transcript_scroll(
+        self, monkeypatch,
+    ):
+        """Dashboard chat must not force inline mode — ScrollBox needs a bounded viewport."""
+        import hermes_cli.main_tui_launch as tui_launch
 
+        monkeypatch.setattr(
+            tui_launch,
+            "_make_tui_argv",
+            lambda project_root, tui_dev=False: (
+                ["node", "dist/entry.js"],
+                "/tmp/ui-tui",
+            ),
+        )
+
+        _argv, _cwd, env = _web_server_chat._resolve_chat_argv()
+
+        assert env is not None
+        assert "HERMES_TUI_INLINE" not in env
+        assert env["HERMES_TUI_DISABLE_MOUSE"] == "1"
+        assert env["HERMES_TUI_DASHBOARD"] == "1"
 
     def test_tui_python_command_uses_child_path(self, tmp_path):
         """Bare Python commands are resolved from the TUI child's PATH."""
@@ -4606,10 +4626,6 @@ class TestPtyWebSocket:
         main_tui_launch._apply_tui_python_env(env)
 
         assert env["HERMES_PYTHON"] == command
-
-
-
-
 
     def test_resolve_chat_argv_async_uses_worker_thread(self, monkeypatch):
         captured: dict = {}
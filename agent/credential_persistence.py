Carrie111998/Hermes

fix(credentials): close the review points on the borrowed-credential rotation check
Three follow-ups from review of the borrowed-credential cooldown fix.

Field drift is no longer a silent hazard. The stored side is a single
fingerprint of whichever field _credential_secret_fingerprint preferred at
write time, so comparing it against the incoming payload's single preferred
fingerprint reintroduced the every-load rotation the moment a borrowed payload
grew a higher-priority secret field. credential_secret_fingerprints() now
returns every fingerprint a payload's secret fields can produce, and the
comparison accepts a match on any of them: a payload that starts carrying an
agent_key beside an unchanged access_token still matches on the access_token,
while a genuinely rotated secret matches nothing and is still a rotation.

extra is read defensively as (existing.extra or {}) — __post_init__ normalizes
it today, but the comparison must not be the thing that breaks if some future
construction path leaves it None.

Both unknown cases now log at debug. "Stored fingerprint, nothing
fingerprintable incoming" keeps the exhaustion state by policy; that is
indistinguishable from a comparison that failed by accident unless it says so.
The rotation branch logs too, so a cleared cooldown is traceable to a decision.

The owned-source short-circuit that sanitize_borrowed_credential_payload used
to provide implicitly is now an explicit is_borrowed_credential_source() guard,
since the fingerprint set is computed from the raw payload.

Tests: 4 added — field drift is not a rotation, a real key change still is, the
unfingerprintable case logs its reason, and a None extra does not raise.

  python -m pytest tests/agent/test_credential_pool_borrowed_status_persistence.py \
    tests/agent/test_credential_pool.py tests/hermes_cli/test_auth_commands.py -q
  85 passed in 6.32s

Co-Authored-By: Claude <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01LSgV4p6D2LhiiRCzeykZ

agent/credential_persistence.py
@@ -130,8 +130,11 @@ def _fingerprint_value(value: Any) -> str | None:
     return f"sha256:{digest[:16]}"
 
 
+_PREFERRED_SECRET_KEYS = ("agent_key", "access_token", "refresh_token", "api_key", "token", "secret")
+
+
 def _credential_secret_fingerprint(payload: Mapping[str, Any]) -> str | None:
-    for key in ("agent_key", "access_token", "refresh_token", "api_key", "token", "secret"):
+    for key in _PREFERRED_SECRET_KEYS:
         fingerprint = _fingerprint_value(payload.get(key))
         if fingerprint:
             return fingerprint
@@ -146,6 +149,35 @@ def _credential_secret_fingerprint(payload: Mapping[str, Any]) -> str | None:
     if isinstance(existing, str) and existing.startswith("sha256:"):
         return existing
     return None
+
+
+def credential_secret_fingerprints(payload: Mapping[str, Any]) -> set[str]:
+    """Every fingerprint the secret-bearing fields of *payload* could produce.
+
+    :func:`_credential_secret_fingerprint` persists exactly one of these —
+    whichever field it prefers.  A caller comparing a stored fingerprint
+    against a fresh payload must accept a match on *any* of them: the preferred
+    field can drift (a borrowed source that starts carrying an ``agent_key``
+    alongside its ``access_token`` fingerprints a different field than the
+    stored entry did), and a set comparison keeps that drift from reading as a
+    secret rotation on every single load.
+    """
+    fingerprints: set[str] = set()
+    for key in _PREFERRED_SECRET_KEYS:
+        fingerprint = _fingerprint_value(payload.get(key))
+        if fingerprint:
+            fingerprints.add(fingerprint)
+
+    for key, value in payload.items():
+        if _is_secret_payload_key(key):
+            fingerprint = _fingerprint_value(value)
+            if fingerprint:
+                fingerprints.add(fingerprint)
+
+    existing = payload.get("secret_fingerprint")
+    if isinstance(existing, str) and existing.startswith("sha256:"):
+        fingerprints.add(existing)
+    return fingerprints
 
 
 def sanitize_borrowed_credential_payload(

NousResearch/hermes-agent

Co-Authored-By: Devin <158243242+devin-ai-integration[bot]@users.noreply.github.com>

agent/credential_persistence.py
@@ -82,9 +82,11 @@ def fingerprint_secret_value(value: Any) -> str | None:
     return f"sha256:{digest[:16]}"
 
 
+_PREFERRED_SECRET_KEYS = ("agent_key", "access_token", "refresh_token", "api_key", "token", "secret")
+
+
 def _credential_secret_fingerprint(payload: Mapping[str, Any]) -> str | None:
-    preferred = ("agent_key", "access_token", "refresh_token", "api_key", "token", "secret")
-    candidates = [payload.get(k) for k in preferred]
+    candidates = [payload.get(k) for k in _PREFERRED_SECRET_KEYS]
     candidates += [v for k, v in payload.items() if _is_secret_payload_key(k)]
     for value in candidates:
         fingerprint = fingerprint_secret_value(value)
@@ -94,6 +96,35 @@ def _credential_secret_fingerprint(payload: Mapping[str, Any]) -> str | None:
     if isinstance(existing, str) and existing.startswith("sha256:"):
         return existing
     return None
+
+
+def credential_secret_fingerprints(payload: Mapping[str, Any]) -> set[str]:
+    """Every fingerprint the secret-bearing fields of *payload* could produce.
+
+    :func:`_credential_secret_fingerprint` persists exactly one of these —
+    whichever field it prefers.  A caller comparing a stored fingerprint
+    against a fresh payload must accept a match on *any* of them: the preferred
+    field can drift (a borrowed source that starts carrying an ``agent_key``
+    alongside its ``access_token`` fingerprints a different field than the
+    stored entry did), and a set comparison keeps that drift from reading as a
+    secret rotation on every single load.
+    """
+    fingerprints: set[str] = set()
+    for key in _PREFERRED_SECRET_KEYS:
+        fingerprint = fingerprint_secret_value(payload.get(key))
+        if fingerprint:
+            fingerprints.add(fingerprint)
+
+    for key, value in payload.items():
+        if _is_secret_payload_key(key):
+            fingerprint = fingerprint_secret_value(value)
+            if fingerprint:
+                fingerprints.add(fingerprint)
+
+    existing = payload.get("secret_fingerprint")
+    if isinstance(existing, str) and existing.startswith("sha256:"):
+        fingerprints.add(existing)
+    return fingerprints
 
 
 def sanitize_borrowed_credential_payload(
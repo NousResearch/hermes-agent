Carrie111998/Hermes

fix(hindsight): address review feedback for invalidate tool
Address all three blocking issues from teknium1's review on #68206:

1. Recall defaulted to observation-only, making the recall→invalidate
   workflow a dead end — added optional 'types' param to hindsight_recall
   tool schema so agents can specify ['world','experience'] when looking
   for curatable facts.

2. Truncated ID display (r.id[:12]) while full ID was sent to API —
   now returns the full r.id from recall results. Also fixed a latent
   bug where _try_import_update_memory_request() was called as bare name
   instead of self._try_import_update_memory_request().

3. No tests + existing tests broken by r.id access on text-only fixture —
   updated mock fixture with id and state fields, updated existing tests,
   added 5 new tests covering:
   - types override vs default
   - [INVALIDATED] flag display
   - missing id graceful fallback
   - invalidate (SDK path, HTTP fallback, restore, error handling)

Also updated README.md and website docs with the new tool.

plugins/memory/hindsight/__init__.py
@@ -315,12 +315,30 @@ RECALL_SCHEMA = {
     "name": "hindsight_recall",
     "description": (
         "Search long-term memory. Returns memories ranked by relevance using "
-        "semantic search, keyword matching, entity graph traversal, and reranking."
+        "semantic search, keyword matching, entity graph traversal, and reranking.\n\n"
+        "Each result includes an id=... prefix so the target can be passed to "
+        "hindsight_invalidate. Use the optional `types` parameter to recall "
+        "world/experience facts (curatable) instead of the default observations.\n\n"
+        "FACT TYPES: observation (consolidated summaries), world (external "
+        "knowledge), experience (agent's own actions). Only world/experience "
+        "facts can be invalidated."
     ),
     "parameters": {
         "type": "object",
         "properties": {
             "query": {"type": "string", "description": "What to search for."},
+            "types": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": ["world", "experience", "observation"],
+                },
+                "description": (
+                    "Fact types to recall. Overrides the configured default "
+                    "for this single call. Use ['world','experience'] when "
+                    "looking for facts to invalidate. Omit to use defaults."
+                ),
+            },
         },
         "required": ["query"],
     },
@@ -358,7 +376,7 @@ INVALIDATE_SCHEMA = {
         "properties": {
             "memory_id": {
                 "type": "string",
-                "description": "Memory ID from hindsight_recall results (e.g. '5e79c849-f3b6')."
+                "description": "Full memory ID from hindsight_recall results (e.g. from 'id=5e79c849-f3b6-4a1e-b789-123456789abc' output)."
             },
             "reason": {
                 "type": "string",
@@ -1771,8 +1789,12 @@ class HindsightMemoryProvider(MemoryProvider):
                 if self._recall_tags:
                     recall_kwargs["tags"] = self._recall_tags
                     recall_kwargs["tags_match"] = self._recall_tags_match
-                if self._recall_types:
-                    recall_kwargs["types"] = self._recall_types
+                # Agent-provided `types` param overrides the configured default
+                # for this single call. Omit → fall back to self._recall_types.
+                caller_types = args.get("types")
+                effective_types = caller_types if caller_types else self._recall_types
+                if effective_types:
+                    recall_kwargs["types"] = effective_types
                 logger.debug("Tool hindsight_recall: bank=%s, query_len=%d, budget=%s",
                              self._bank_id, len(query), self._budget)
                 resp = self._run_hindsight_operation(lambda client: client.arecall(**recall_kwargs))
@@ -1782,10 +1804,11 @@ class HindsightMemoryProvider(MemoryProvider):
                     return json.dumps({"result": "No relevant memories found."})
                 lines = []
                 for i, r in enumerate(resp.results, 1):
-                    sid = r.id[:12] if r.id else "?"
+                    sid = getattr(r, "id", None)
+                    sid_str = sid if sid else "?"
                     state = getattr(r, "state", None)
                     flag = " [INVALIDATED]" if state == "invalidated" else ""
-                    lines.append(f"{i}. id={sid} {r.text}{flag}")
+                    lines.append(f"{i}. id={sid_str} {r.text}{flag}")
                 return json.dumps({"result": "\n".join(lines)})
             except Exception as e:
                 logger.warning("hindsight_recall failed: %s", e, exc_info=True)
@@ -1817,7 +1840,7 @@ class HindsightMemoryProvider(MemoryProvider):
             reason = args.get("reason", "")
 
             try:
-                UpdateMemoryRequest = _try_import_update_memory_request()
+                UpdateMemoryRequest = self._try_import_update_memory_request()
                 if UpdateMemoryRequest is not None:
                     # —— SDK >= 0.8.4 path ——
                     req = UpdateMemoryRequest(state=state)
@@ -1835,7 +1858,7 @@ class HindsightMemoryProvider(MemoryProvider):
                     self._http_patch_memory(memory_id, state, reason=reason or None)
 
                 action = "restored" if state == "valid" else "invalidated"
-                return json.dumps({"result": f"Memory {memory_id[:12]}... {action}."})
+                return json.dumps({"result": f"Memory {memory_id} {action}."})
             except Exception as e:
                 logger.warning("hindsight_invalidate failed: %s", e, exc_info=True)
                 return tool_error(f"Failed to curate memory: {e}")

NousResearch/hermes-agent

plugins/memory/hindsight/__init__.py
@@ -238,10 +238,33 @@ RECALL_SCHEMA = {
     "name": "hindsight_recall",
     "description": (
         "Search long-term memory. Returns memories ranked by relevance using "
-        "semantic search, keyword matching, entity graph traversal, and reranking."
+        "semantic search, keyword matching, entity graph traversal, and reranking.\n\n"
+        "Each result includes an id=... prefix so the target can be passed to "
+        "hindsight_invalidate. Use the optional `types` parameter to recall "
+        "world/experience facts (curatable) instead of the default observations.\n\n"
+        "FACT TYPES: observation (consolidated summaries), world (external "
+        "knowledge), experience (agent's own actions). Only world/experience "
+        "facts can be invalidated."
     ),
-    "parameters": {"type": "object", "required": ["query"],
-                   "properties": {"query": {"type": "string", "description": "What to search for."}}},
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {"type": "string", "description": "What to search for."},
+            "types": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": ["world", "experience", "observation"],
+                },
+                "description": (
+                    "Fact types to recall. Overrides the configured default "
+                    "for this single call. Use ['world','experience'] when "
+                    "looking for facts to invalidate. Omit to use defaults."
+                ),
+            },
+        },
+        "required": ["query"],
+    },
 }
 
 REFLECT_SCHEMA = {
@@ -271,7 +294,7 @@ INVALIDATE_SCHEMA = {
         "properties": {
             "memory_id": {
                 "type": "string",
-                "description": "Memory ID from hindsight_recall results (e.g. '5e79c849-f3b6')."
+                "description": "Full memory ID from hindsight_recall results (e.g. from 'id=5e79c849-f3b6-4a1e-b789-123456789abc' output)."
             },
             "reason": {
                 "type": "string",
@@ -908,12 +931,14 @@ class HindsightMemoryProvider(MemoryProvider):
             logger.debug("Prefetch: skipped (%s)", why)
         return why is not None
 
-    def _recall(self, query: str) -> list:
+    def _recall(self, query: str, types: list | None = None) -> list:
         kwargs: dict = {"bank_id": self._bank_id, "query": query, "budget": self._budget, "max_tokens": self._recall_max_tokens}
         if self._recall_tags:
             kwargs.update(tags=self._recall_tags, tags_match=self._recall_tags_match)
-        if self._recall_types:
-            kwargs["types"] = self._recall_types
+        # Agent-provided `types` (per call) overrides the configured default.
+        effective_types = types or self._recall_types
+        if effective_types:
+            kwargs["types"] = effective_types
         resp = self._run_hindsight_operation(lambda client: client.arecall(**kwargs))
         return resp.results or []
 
@@ -1135,16 +1160,19 @@ class HindsightMemoryProvider(MemoryProvider):
         query = args["query"]
         logger.debug("Tool hindsight_recall: bank=%s, query_len=%d, budget=%s",
                      self._bank_id, len(query), self._budget)
-        results = self._recall(query)
+        # Agent-provided `types` param overrides the configured default
+        # for this single call. Omit → fall back to self._recall_types.
+        results = self._recall(query, types=args.get("types"))
         logger.debug("Tool hindsight_recall: %d results", len(results))
         if not results:
             return "No relevant memories found."
         lines = []
         for i, r in enumerate(results, 1):
-            sid = r.id[:12] if r.id else "?"
+            sid = getattr(r, "id", None)
+            sid_str = sid if sid else "?"
             state = getattr(r, "state", None)
             flag = " [INVALIDATED]" if state == "invalidated" else ""
-            lines.append(f"{i}. id={sid} {r.text}{flag}")
+            lines.append(f"{i}. id={sid_str} {r.text}{flag}")
         return "\n".join(lines)
 
     def _tool_reflect(self, args: dict) -> str:
@@ -1178,7 +1206,7 @@ class HindsightMemoryProvider(MemoryProvider):
             self._http_patch_memory(memory_id, state, reason=reason or None)
 
         action = "restored" if state == "valid" else "invalidated"
-        return f"Memory {memory_id[:12]}... {action}."
+        return f"Memory {memory_id} {action}."
 
     # tool name -> (required arg, handler, user-facing failure prefix)
     _TOOL_HANDLERS = {
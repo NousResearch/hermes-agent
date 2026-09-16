Carrie111998/Hermes

fix(desktop): keep warm clarification actionable
Project the authoritative pending clarify before persisted transcript hydration, preserve request identity across activation races, and keep stale or synthetic clarification liveness out of visible and durable state.\n\nPorted as the stable net patch from 4d72dadf287dd4b2743e3e161f56bcdcbcd94fd8 onto upstream.

apps/desktop/src/app/session/hooks/use-session-actions/restore-pending-clarify.ts
@@ -15,6 +15,44 @@ export interface PendingClarifyResumeState {
   request: ClarifyRequest | null
 }
 
+export function pendingClarifyRequestFromSnapshot(
+  response: Pick<SessionResumeResponse, 'pending_clarify'>,
+  sessionId: string
+): ClarifyRequest | null {
+  const pending = response.pending_clarify
+
+  if (!pending || typeof pending.request_id !== 'string') {
+    return null
+  }
+
+  const questions = normalizeQuestions(pending.questions)
+  const question = typeof pending.question === 'string' ? pending.question : ''
+
+  if (!question && questions.length === 0) {
+    return null
+  }
+
+  const choices = normalizeChoices(pending.choices)
+
+  const lockedAnswers =
+    typeof pending.answers === 'object' && pending.answers !== null
+      ? Object.fromEntries(
+          Object.entries(pending.answers).filter((entry): entry is [string, string] => typeof entry[1] === 'string')
+        )
+      : undefined
+
+  return {
+    choices: choices.length > 0 ? choices : null,
+    lockedAnswers,
+    multiSelect: pending.multi_select === true,
+    question,
+    receivedAt: Date.now() / 1000,
+    requestId: pending.request_id,
+    sessionId,
+    ...(questions.length > 0 ? { questions } : {})
+  }
+}
+
 /**
  * Restore a pending clarify from a resume/activate snapshot onto `sessionId`.
  *
@@ -33,8 +71,13 @@ export function restorePendingClarifyFromSnapshot(
   response: Pick<SessionResumeResponse, 'pending_clarify'>,
   sessionId: string,
   resumeStartedAt: number,
-  requestIdAtStart?: string
+  requestIdAtStart?: string | null,
+  preserveCurrentClarifyRequest = false
 ): PendingClarifyResumeState {
+  if (preserveCurrentClarifyRequest) {
+    return { authoritativeAbsent: false, cleared: null, request: null }
+  }
+
   const pending = response.pending_clarify
 
   if (!pending || typeof pending.request_id !== 'string') {
@@ -53,31 +96,10 @@ export function restorePendingClarifyFromSnapshot(
     return { authoritativeAbsent: true, cleared: null, request: null }
   }
 
-  const questions = normalizeQuestions(pending.questions)
-  const question = typeof pending.question === 'string' ? pending.question : ''
+  const request = pendingClarifyRequestFromSnapshot(response, sessionId)
 
-  if (!question && questions.length === 0) {
+  if (!request) {
     return { authoritativeAbsent: false, cleared: null, request: null }
-  }
-
-  const choices = normalizeChoices(pending.choices)
-
-  const lockedAnswers =
-    typeof pending.answers === 'object' && pending.answers !== null
-      ? Object.fromEntries(
-          Object.entries(pending.answers).filter((entry): entry is [string, string] => typeof entry[1] === 'string')
-        )
-      : undefined
-
-  const request: ClarifyRequest = {
-    choices: choices.length > 0 ? choices : null,
-    lockedAnswers,
-    multiSelect: pending.multi_select === true,
-    question,
-    receivedAt: Date.now() / 1000,
-    requestId: pending.request_id,
-    sessionId,
-    ...(questions.length > 0 ? { questions } : {})
   }
 
   setClarifyRequest(request)

NousResearch/hermes-agent

apps/desktop/src/app/session/hooks/use-session-actions/restore-pending-clarify.ts
@@ -1,11 +1,61 @@
 import type { GatewayEventPayload } from '@/lib/chat-messages'
-import { $clarifyRequests, type ClarifyRequest, clearClarifyRequest } from '@/store/clarify'
+import {
+  $clarifyRequests,
+  type ClarifyRequest,
+  clearClarifyRequest,
+  normalizeChoices,
+  normalizeQuestions
+} from '@/store/clarify'
 import type { SessionResumeResult } from '@/types/hermes'
 
 export interface PendingClarifyResumeState {
   authoritativeAbsent: boolean
   cleared: ClarifyRequest | null
   request: ClarifyRequest | null
+}
+
+export function pendingClarifyRequestFromSnapshot(
+  response: Pick<SessionResumeResult, 'open_requests'>,
+  sessionId: string
+): ClarifyRequest | null {
+  const pending = (response.open_requests ?? []).find(entry => entry.method === 'clarify')
+
+  if (!pending) {
+    return null
+  }
+
+  const parked = $clarifyRequests.get()[sessionId]
+
+  if (parked?.requestId === pending.id) {
+    return parked
+  }
+
+  const questions = normalizeQuestions(pending.params.questions)
+  const question = typeof pending.params.question === 'string' ? pending.params.question : ''
+
+  if (!question && questions.length === 0) {
+    return null
+  }
+
+  const choices = normalizeChoices(pending.params.choices)
+  const answers = pending.params.answers
+  const lockedAnswers =
+    typeof answers === 'object' && answers !== null
+      ? Object.fromEntries(
+          Object.entries(answers).filter((entry): entry is [string, string] => typeof entry[1] === 'string')
+        )
+      : undefined
+
+  return {
+    choices: choices.length > 0 ? choices : null,
+    lockedAnswers,
+    multiSelect: pending.params.multi_select === true,
+    question,
+    receivedAt: Date.now() / 1000,
+    requestId: pending.id,
+    sessionId,
+    ...(questions.length > 0 ? { questions } : {})
+  }
 }
 
 /**
@@ -24,8 +74,13 @@ export function restorePendingClarifyFromSnapshot(
   response: Pick<SessionResumeResult, 'open_requests'>,
   sessionId: string,
   resumeStartedAt: number,
-  requestIdAtStart?: string
+  requestIdAtStart?: string | null,
+  preserveCurrentClarifyRequest = false
 ): PendingClarifyResumeState {
+  if (preserveCurrentClarifyRequest) {
+    return { authoritativeAbsent: false, cleared: null, request: null }
+  }
+
   const pending = (response.open_requests ?? []).find(entry => entry.method === 'clarify')
 
   if (!pending) {
@@ -48,8 +103,9 @@ export function restorePendingClarifyFromSnapshot(
   // re-delivered `open_requests`; a card the handler declined (empty
   // question) is simply not there.
   const parked = $clarifyRequests.get()[sessionId]
+  const request = parked?.requestId === pending.id ? parked : null
 
-  return { authoritativeAbsent: false, cleared: null, request: parked?.requestId === pending.id ? parked : null }
+  return { authoritativeAbsent: false, cleared: null, request }
 }
 
 export function pendingClarifyToolPayload(request: ClarifyRequest): GatewayEventPayload {
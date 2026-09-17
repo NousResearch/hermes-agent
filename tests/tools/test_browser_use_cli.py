Gdheubs/hermes

fix(browser): decode Browser Use CLI stdout as UTF-8
browser_exec ran subprocess with text=True and no encoding=. On Windows
that uses the locale preferred encoding (cp1252/cp936), so UTF-8 CLI
output with non-ASCII bytes kills the reader thread and returns empty
tool output. Match install_cli and pass encoding=utf-8, errors=replace.

Fixes #87152

Signed-off-by: Olympusbuildz <Olympus.roots@outlook.com>
Co-authored-by: Olympusbuildz <Olympus.roots@outlook.com>
Signed-off-by: Olympusbuildz <Olympus.roots@outlook.com>

tests/tools/test_browser_use_cli.py
@@ -835,6 +835,43 @@ class TestBrowserExec:
         result = json.loads(bu_cli.browser_exec("print(1)", timeout_s=1))
         assert "timed out" in result["error"]
 
+    def test_exec_subprocess_decodes_stdout_as_utf8(self, tmp_path, monkeypatch):
+        """Windows text=True defaults to locale encoding; CLI emits UTF-8 (#87152)."""
+        cli = _fake_cli(tmp_path, "cat > /dev/null\necho ok\n")
+        monkeypatch.setattr(bu_cli, "_find_cli", lambda: [cli])
+        captured = {}
+        real_run = bu_cli.subprocess.run
+
+        def _spy(*args, **kwargs):
+            captured.update(kwargs)
+            return real_run(*args, **kwargs)
+
+        monkeypatch.setattr(bu_cli.subprocess, "run", _spy)
+        result = json.loads(bu_cli.browser_exec("print(1)"))
+        assert result["success"] is True
+        assert captured.get("encoding") == "utf-8"
+        assert captured.get("errors") == "replace"
+        assert captured.get("text") is True
+
+    def test_exec_preserves_non_ascii_utf8_stdout(self, tmp_path, monkeypatch):
+        # Euro + em dash: UTF-8 bytes that crash cp1252 if decoded as locale.
+        import stat as _stat
+
+        cli = tmp_path / "browser-use-utf8"
+        body = (
+            "#!/usr/bin/env python3\n"
+            "import sys\n"
+            "sys.stdin.read()\n"
+            "sys.stdout.buffer.write('price \u20ac9 \u2014 done\\n'.encode('utf-8'))\n"
+        )
+        cli.write_text(body, encoding="utf-8")
+        cli.chmod(cli.stat().st_mode | _stat.S_IXUSR)
+        monkeypatch.setattr(bu_cli, "_find_cli", lambda: [str(cli)])
+        result = json.loads(bu_cli.browser_exec("print(1)"))
+        assert result["success"] is True
+        assert "\u20ac9" in result["output"]
+        assert "\u2014" in result["output"]
+
 
 class TestFindCliManagedBin:
     """MANAGED-FIRST: _find_cli probes $HERMES_HOME/bin before PATH and

NousResearch/hermes-agent

tests/tools/test_browser_use_cli.py
@@ -1011,6 +1011,47 @@ class TestBrowserExec:
         result = json.loads(bu_cli.browser_exec("print(1)", timeout_s=1))
         assert "timed out" in result["error"]
 
+    def test_exec_subprocess_decodes_stdout_as_utf8(self, tmp_path, monkeypatch):
+        """Windows text=True defaults to locale encoding; CLI emits UTF-8 (#87152).
+
+        browser_exec now runs via _run_cli_killing_process_group -> subprocess.Popen
+        (not subprocess.run); assert the Popen kwargs keep utf-8/replace.
+        """
+        cli = _fake_cli(tmp_path, "cat > /dev/null\necho ok\n")
+        monkeypatch.setattr(bu_cli, "_find_cli", lambda: [cli])
+        captured = {}
+        real_popen = bu_cli.subprocess.Popen
+
+        def _spy(*args, **kwargs):
+            captured.update(kwargs)
+            return real_popen(*args, **kwargs)
+
+        monkeypatch.setattr(bu_cli.subprocess, "Popen", _spy)
+        result = json.loads(bu_cli.browser_exec("print(1)"))
+        assert result["success"] is True
+        assert captured.get("encoding") == "utf-8"
+        assert captured.get("errors") == "replace"
+        assert captured.get("text") is True
+
+    def test_exec_preserves_non_ascii_utf8_stdout(self, tmp_path, monkeypatch):
+        # Euro + em dash: UTF-8 bytes that crash cp1252 if decoded as locale.
+        import stat as _stat
+
+        cli = tmp_path / "browser-use-utf8"
+        body = (
+            "#!/usr/bin/env python3\n"
+            "import sys\n"
+            "sys.stdin.read()\n"
+            "sys.stdout.buffer.write('price \u20ac9 \u2014 done\\n'.encode('utf-8'))\n"
+        )
+        cli.write_text(body, encoding="utf-8")
+        cli.chmod(cli.stat().st_mode | _stat.S_IXUSR)
+        monkeypatch.setattr(bu_cli, "_find_cli", lambda: [str(cli)])
+        result = json.loads(bu_cli.browser_exec("print(1)"))
+        assert result["success"] is True
+        assert "\u20ac9" in result["output"]
+        assert "\u2014" in result["output"]
+
 
 class TestFindCliManagedBin:
     """MANAGED-FIRST: _find_cli probes $HERMES_HOME/bin before PATH and
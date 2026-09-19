bottombgr-design/hermes

feat(slack): configurable slash-command namespace prefix
Slack slash commands are workspace-global and not namespaced. When two
gateway apps (or Hermes alongside another app using generic names like
/model, /new, /help) are installed in the same workspace, Slack routes
each shared command to whichever app was installed most recently,
silently breaking the others.

Add an opt-in namespace prefix so every command can be namespaced (e.g.
/myorg-model, /myorg-hermes) while the gateway still recognizes them.

- slack_command_prefix() resolves platforms.slack.extra.command_prefix
  from config.yaml. AGENTS.md reserves HERMES_* env vars for secrets, so
  behavioral settings live in config only. Default "" is a no-op.
- The manifest prepends the prefix to every generated command. Names
  pushed past Slack's 32-char limit are skipped with a warning; they stay
  reachable via /<prefix>hermes <subcommand>.
- The adapter bakes the prefix into the slash-routing regex and strips it
  from slash_name before the {"hermes", ""} check and registry dispatch.
- Remaining command surfaces are prefix-aware: the in-thread ! fallback
  strips the prefix before dispatch, alias descriptions reference the
  prefixed names, the built-in reserved-name filter checks the full
  registered name (so /myorg-status gains a native slot), and a prefix
  that sanitizes to empty warns instead of silently disabling namespacing.

Closes #66156.

hermes_cli/slack_cli.py
@@ -18,6 +18,7 @@ for reinstall when scopes/commands change.
 from __future__ import annotations
 
 import json
+import logging
 import os
 import sys
 from pathlib import Path
@@ -238,17 +239,29 @@ def slack_manifest_command(args) -> int:
     else:
         messaging_experience = "assistant"
 
-    if getattr(args, "slashes_only", False):
-        from hermes_cli.commands import slack_app_manifest
+    # CLI logging writes only to rotating log files (no console handler), so
+    # mirror manifest-generation warnings — e.g. commands skipped for
+    # exceeding Slack's name-length limit — to stderr where the operator can
+    # see them. stdout stays reserved for the manifest JSON.
+    commands_logger = logging.getLogger("hermes_cli.commands")
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.WARNING)
+    stderr_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+    commands_logger.addHandler(stderr_handler)
+    try:
+        if getattr(args, "slashes_only", False):
+            from hermes_cli.commands import slack_app_manifest
 
-        manifest = slack_app_manifest()["features"]["slash_commands"]
-    else:
-        manifest = _build_full_manifest(
-            name,
-            description,
-            messaging_experience=messaging_experience,
-            long_description=long_description,
-        )
+            manifest = slack_app_manifest()["features"]["slash_commands"]
+        else:
+            manifest = _build_full_manifest(
+                name,
+                description,
+                messaging_experience=messaging_experience,
+                long_description=long_description,
+            )
+    finally:
+        commands_logger.removeHandler(stderr_handler)
 
     payload = json.dumps(manifest, indent=2, ensure_ascii=False) + "\n"
 

tests/hermes_cli/test_slack_cli.py
@@ -153,3 +153,36 @@ class TestSlackFullManifest:
             assert event in bot_events
 
 
+class TestSlackManifestWarnings:
+    """`hermes slack manifest` surfaces generation warnings on stderr."""
+
+    def test_over_limit_skip_warning_reaches_stderr(self, monkeypatch, capsys):
+        from hermes_cli import commands as cmds
+        from hermes_cli.slack_cli import slack_manifest_command
+
+        monkeypatch.setattr(
+            "hermes_cli.config.read_raw_config",
+            lambda: {"platforms": {"slack": {"extra": {"command_prefix": "myorg-"}}}},
+        )
+        long_name = "x" * 30  # 30 + len("myorg-") = 36 > 32-char Slack limit
+        monkeypatch.setattr(
+            cmds,
+            "slack_native_slashes",
+            lambda prefix="": [("hermes", "d", ""), (long_name, "d", "")],
+        )
+
+        rc = slack_manifest_command(argparse.Namespace(slashes_only=True))
+
+        assert rc == 0
+        captured = capsys.readouterr()
+        # hermes' CLI logging has no console handler, so the CLI must mirror
+        # the skip warning to stderr itself; stdout stays valid JSON.
+        assert f"myorg-{long_name}" in captured.err
+        assert json.loads(captured.out) == [
+            {
+                "command": "/myorg-hermes",
+                "description": "d",
+                "should_escape": False,
+                "url": "https://hermes-agent.local/slack/commands",
+            }
+        ]

NousResearch/hermes-agent

hermes_cli/slack_cli.py
@@ -3,6 +3,7 @@ command as a native Slack slash (``/btw``, ``/stop``, ``/model``, …)."""
 from __future__ import annotations
 
 import json
+import logging
 import sys
 from pathlib import Path
 
@@ -112,13 +113,24 @@ def slack_manifest_command(args) -> int:
     else:
         messaging_experience = "assistant"
 
-    if slashes_only:
-        from hermes_cli.commands_platforms import slack_app_manifest
-        manifest = slack_app_manifest()["features"]["slash_commands"]
-    else:
-        manifest = _build_full_manifest(
-            name, description, messaging_experience=messaging_experience,
-            long_description=long_description)
+    # CLI logging writes only to rotating log files (no console handler), so mirror
+    # manifest-generation warnings — e.g. commands skipped for exceeding Slack's name-length
+    # limit — to stderr where the operator can see them. stdout stays reserved for the JSON.
+    commands_logger = logging.getLogger("hermes_cli.commands")
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.WARNING)
+    stderr_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+    commands_logger.addHandler(stderr_handler)
+    try:
+        if slashes_only:
+            from hermes_cli.commands_platforms import slack_app_manifest
+            manifest = slack_app_manifest()["features"]["slash_commands"]
+        else:
+            manifest = _build_full_manifest(
+                name, description, messaging_experience=messaging_experience,
+                long_description=long_description)
+    finally:
+        commands_logger.removeHandler(stderr_handler)
     payload = json.dumps(manifest, indent=2, ensure_ascii=False) + "\n"
 
     write_target = getattr(args, "write", None)

tests/hermes_cli/test_slack_cli.py
@@ -154,3 +154,36 @@ class TestSlackFullManifest:
             assert event in bot_events
 
 
+class TestSlackManifestWarnings:
+    """`hermes slack manifest` surfaces generation warnings on stderr."""
+
+    def test_over_limit_skip_warning_reaches_stderr(self, monkeypatch, capsys):
+        from hermes_cli import commands_platforms as cmds
+        from hermes_cli.slack_cli import slack_manifest_command
+
+        monkeypatch.setattr(
+            "hermes_cli.config.read_raw_config",
+            lambda: {"platforms": {"slack": {"extra": {"command_prefix": "myorg-"}}}},
+        )
+        long_name = "x" * 30  # 30 + len("myorg-") = 36 > 32-char Slack limit
+        monkeypatch.setattr(
+            cmds,
+            "slack_native_slashes",
+            lambda prefix="": [("hermes", "d", ""), (long_name, "d", "")],
+        )
+
+        rc = slack_manifest_command(argparse.Namespace(slashes_only=True))
+
+        assert rc == 0
+        captured = capsys.readouterr()
+        # hermes' CLI logging has no console handler, so the CLI must mirror
+        # the skip warning to stderr itself; stdout stays valid JSON.
+        assert f"myorg-{long_name}" in captured.err
+        assert json.loads(captured.out) == [
+            {
+                "command": "/myorg-hermes",
+                "description": "d",
+                "should_escape": False,
+                "url": "https://hermes-agent.local/slack/commands",
+            }
+        ]
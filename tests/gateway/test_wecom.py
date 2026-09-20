Carrie111998/Hermes

fix(gateway): classify padded slash commands as COMMAND on weixin/wecom
Command detection is boundary-aware now, but the inbound classifiers that
tag MessageType.COMMAND were not, so commands still took the wrong route
into the pipeline.

Both adapters send only TEXT through _enqueue_text_event, which holds the
event for a debounce window and concatenates whatever arrives next. A
command misclassified as TEXT therefore waits out the quiet period and can
have the user's follow-up glued on as its arguments: "/deny" followed by
"never mind" reaches the handler as a deny reason.

- weixin._message_type_from_media used a bare startswith("/"), so
  "⁠/deny" fell to TEXT.
- wecom._derive_message_type never checked for "/" at all, so even a plain
  "/deny" fell to TEXT — its caller's comment ("commands, media, etc.
  dispatch immediately") always assumed a branch that was missing. WeCom is
  where the original report came from.

Both now call looks_like_slash_command(), the predicate introduced in the
previous commit, so classification and detection share one source of truth
instead of drifting again.

Media and voice keep priority over command text; this only changes the
plain-text fallthrough.

tests/gateway/test_wecom.py
@@ -483,3 +483,39 @@ class TestTextBatchFlushRace:
             "superseded task must not pop the event"
         )
 
+
+class TestWeComCommandClassification:
+    """Commands must classify as COMMAND so they skip text batching.
+
+    _process_message sends only TEXT through _enqueue_text_event, whose
+    debounce window merges a pending command with the next message — so a
+    "/deny" followed by "never mind" would arrive as a deny reason.
+    """
+
+    @staticmethod
+    def _derive(text: str, media_types=None):
+        from plugins.platforms.wecom.adapter import WeComAdapter
+        from gateway.platforms.base import MessageType
+
+        return WeComAdapter._derive_message_type({}, text, media_types or []), MessageType
+
+    def test_plain_command_classifies_as_command(self):
+        result, MessageType = self._derive("/deny")
+        assert result == MessageType.COMMAND
+
+    def test_invisible_padded_command_classifies_as_command(self):
+        result, MessageType = self._derive("⁠/deny⁠")
+        assert result == MessageType.COMMAND
+
+    def test_plain_text_still_classifies_as_text(self):
+        result, MessageType = self._derive("hello")
+        assert result == MessageType.TEXT
+
+    def test_voice_still_wins_over_command_text(self):
+        from plugins.platforms.wecom.adapter import WeComAdapter
+        from gateway.platforms.base import MessageType
+
+        assert (
+            WeComAdapter._derive_message_type({"msgtype": "voice"}, "/deny", [])
+            == MessageType.VOICE
+        )

NousResearch/hermes-agent

tests/gateway/test_wecom.py
@@ -1601,3 +1601,40 @@ class TestFinalFrameAckTimeoutSemantics:
                 {"msgtype": "stream", "stream": {"id": "stream_x", "content": "x", "finish": True}},
                 is_final=True,
             )
+
+
+class TestWeComCommandClassification:
+    """Commands must classify as COMMAND so they skip text batching.
+
+    _process_message sends only TEXT through _enqueue_text_event, whose
+    debounce window merges a pending command with the next message — so a
+    "/deny" followed by "never mind" would arrive as a deny reason.
+    """
+
+    @staticmethod
+    def _derive(text: str, media_types=None):
+        from plugins.platforms.wecom.adapter import WeComAdapter
+        from gateway.platforms.event import MessageType
+
+        return WeComAdapter._derive_message_type({}, text, media_types or []), MessageType
+
+    def test_plain_command_classifies_as_command(self):
+        result, MessageType = self._derive("/deny")
+        assert result == MessageType.COMMAND
+
+    def test_invisible_padded_command_classifies_as_command(self):
+        result, MessageType = self._derive("⁠/deny⁠")
+        assert result == MessageType.COMMAND
+
+    def test_plain_text_still_classifies_as_text(self):
+        result, MessageType = self._derive("hello")
+        assert result == MessageType.TEXT
+
+    def test_voice_still_wins_over_command_text(self):
+        from plugins.platforms.wecom.adapter import WeComAdapter
+        from gateway.platforms.event import MessageType
+
+        assert (
+            WeComAdapter._derive_message_type({"msgtype": "voice"}, "/deny", [])
+            == MessageType.VOICE
+        )
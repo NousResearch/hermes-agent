artisanguillonrenov-creator/Jarvis-Hermes-Base

fix(mcp): require durable OAuth grants after login
Reject finite-lived access tokens that cannot refresh noninteractively,
and fail closed when token state is unreadable. Provider-defined bearer
tokens without expiry metadata stay accepted, and has_cached_tokens()
keeps its any-cached-state meaning for the noninteractive precheck so
existing grants are not stranded from the interactive flow.

Rebuild of #95756 on 5910de20bc9839fdd36e791a9d72ba2c2e722f66.

tools/mcp_oauth.py
@@ -622,6 +622,25 @@ class HermesTokenStorage:
         """True if we have tokens on disk (may be expired)."""
         return self._tokens_path().exists()
 
+    def has_durable_tokens(self) -> bool:
+        """Return whether cached OAuth state can survive access-token expiry.
+
+        A finite-lived access token without a refresh token is not a durable
+        authorization: it can pass an initial MCP probe and then fail
+        non-interactively as soon as the access token expires.  Treat that
+        shape as uncommissioned rather than reporting a false login success.
+        Tokens without expiry metadata may be provider-defined long-lived
+        bearer tokens, so their lack of a refresh token is not rejected here.
+        """
+        data = _read_json(self._tokens_path())
+        if not isinstance(data, dict) or not data.get("access_token"):
+            return False
+        finite_lifetime = (
+            data.get("expires_at") is not None
+            or data.get("expires_in") is not None
+        )
+        return not finite_lifetime or bool(data.get("refresh_token"))
+
 
 # Callback capture: the HTTP listener and the stdin paste reader share one result dict.
 def _authorization_code_result(code: str, state: "str | None", iss: "str | None" = None):

NousResearch/hermes-agent

tools/mcp_oauth.py
@@ -647,6 +647,25 @@ class HermesTokenStorage:
         """True if we have tokens on disk (may be expired)."""
         return self._tokens_path().exists()
 
+    def has_durable_tokens(self) -> bool:
+        """Return whether cached OAuth state can survive access-token expiry.
+
+        A finite-lived access token without a refresh token is not a durable
+        authorization: it will pass an initial MCP probe and then fail
+        non-interactively as soon as the access token expires. Treat that
+        shape as uncommissioned rather than reporting a false login success.
+        Tokens without expiry metadata may be provider-defined long-lived
+        bearer tokens, so their lack of a refresh token is not rejected here.
+        """
+        data = _read_json(self._tokens_path())
+        if not isinstance(data, dict) or not data.get("access_token"):
+            return False
+        finite_lifetime = (
+            data.get("expires_at") is not None
+            or data.get("expires_in") is not None
+        )
+        return not finite_lifetime or bool(data.get("refresh_token"))
+
 
 # Callback capture: the HTTP listener and the stdin paste reader share one result dict.
 def _authorization_code_result(code: str, state: "str | None", iss: "str | None" = None):
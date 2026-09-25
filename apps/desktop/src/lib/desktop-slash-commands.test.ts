Carrie111998/Hermes

fix(desktop): honor catalog /skills allowlist over the static fallback
A live commands catalog that names desktop_subcommands now overlays the
static /skills row, so a newer backend can narrow the review surface.
Old backends with no catalog list still get the hardcoded fallback.
Also treat any whitespace as the argument-stage completion gate, and
assert pending/approve/reject/diff actually answer on the gateway.

apps/desktop/src/lib/desktop-slash-commands.test.ts
@@ -249,10 +249,39 @@ describe('desktop slash command curation', () => {
       'diff <id>',
       'approval'
     ])
+    expect(filterDesktopSubcommandCompletions('/skills\tap', hubItems).map(item => item.text)).toEqual([
+      'pending',
+      'approve <id>',
+      'reject',
+      'diff <id>',
+      'approval'
+    ])
     // Command-token stage (`/skills` itself, no space) passes through.
     expect(filterDesktopSubcommandCompletions('/skills', hubItems)).toHaveLength(hubItems.length)
     // Unrestricted commands keep their full arg completions.
     expect(filterDesktopSubcommandCompletions('/memory ', hubItems)).toHaveLength(hubItems.length)
+  })
+
+  it('lets a live catalog narrow the static /skills allowlist', () => {
+    rememberDesktopCommandsCatalog({
+      commands: {
+        '/skills': {
+          argument_mode: 'options',
+          desktop: null,
+          desktop_subcommands: ['pending', 'approve', 'reject']
+        }
+      },
+      canon: { '/skills': '/skills' }
+    })
+
+    expect(desktopSubcommandAllowlist('/skills')).toEqual(['pending', 'approve', 'reject'])
+    expect(desktopSubcommandUnavailableMessage('/skills', 'diff a1b2')).toContain('/skills diff')
+    expect(desktopSubcommandUnavailableMessage('/skills', 'pending')).toBeNull()
+  })
+
+  it('falls back to the static /skills allowlist when the catalog has none', () => {
+    expect(desktopSubcommandAllowlist('/skills')).toEqual(['pending', 'approve', 'reject', 'diff', 'approval'])
+    expect(desktopSubcommandUnavailableMessage('/skills', 'diff a1b2')).toBeNull()
   })
 
   it('routes /pet through the desktop action handler and drops /pets', () => {

NousResearch/hermes-agent

apps/desktop/src/lib/desktop-slash-commands.test.ts
@@ -125,12 +125,29 @@ describe('desktop slash command curation', () => {
     expect(desktopSubcommandUnavailableMessage('/skills', 'install demo')).toContain('not available')
     expect(desktopSubcommandUnavailableMessage('/skills', 'approve write-1')).toBeNull()
     expect(
-      filterDesktopSubcommandCompletions('/skills ', [
+      filterDesktopSubcommandCompletions('/skills\tap', [
         { text: 'pending' },
         { text: 'install' },
         { text: 'approval on' }
       ])
     ).toEqual([{ text: 'pending' }, { text: 'approval on' }])
+  })
+
+  it('lets the live catalog narrow the static /skills allowlist', () => {
+    rememberDesktopCommandsCatalog({
+      commands: {
+        '/skills': {
+          argument_mode: 'options',
+          desktop: null,
+          desktop_subcommands: ['pending', 'approve', 'reject']
+        }
+      },
+      canon: { '/skills': '/skills' }
+    })
+
+    expect(desktopSubcommandAllowlist('/skills')).toEqual(['pending', 'approve', 'reject'])
+    expect(desktopSubcommandUnavailableMessage('/skills', 'diff write-1')).toContain('/skills diff')
+    expect(desktopSubcommandUnavailableMessage('/skills', 'pending')).toBeNull()
   })
 
   it('routes /compress through the session-compression action', () => {
Carrie111998/Hermes

fix: sort model IDs returned by custom /v1/models probing
probe_api_models() returned the provider's raw /models response order
unchanged, so the /model picker showed a different, non-alphabetical
ordering on every session and cache refresh for custom OpenAI-compatible
endpoints. Built-in providers (anthropic, xai, github) already sort; this
brings the generic custom path in line.

Filter empty IDs and stable-sort by lowercase ID.

hermes_cli/models.py
@@ -5725,8 +5725,17 @@ def probe_api_models(
         try:
             with _urlopen_model_catalog_request(req, **_open_kwargs) as resp:
                 data = json.loads(resp.read().decode())
+                models = [m.get("id", "") for m in data.get("data", [])]
+                # Filter empty IDs and sort case-insensitively so the
+                # /model picker renders a stable, alphabetical order.
+                # Built-in providers (anthropic/xai/github) already sort
+                # their catalogs; this custom OpenAI-compatible path did
+                # not, so the ordering changed with every /v1/models
+                # response and cache refresh.
+                models = [m for m in models if m]
+                models = sorted(models, key=str.lower)
                 return {
-                    "models": [m.get("id", "") for m in data.get("data", [])],
+                    "models": models,
                     "probed_url": url,
                     "resolved_base_url": candidate_base.rstrip("/"),
                     "suggested_base_url": alternate_base if alternate_base != candidate_base else normalized,

NousResearch/hermes-agent

hermes_cli/models.py
@@ -2582,9 +2582,17 @@ def probe_api_models(
         for item in data.get("data", []):
             if isinstance(item, dict) and note_catalog_item(item):
                 continue
-            probed.append(item.get("id", ""))
+            probed.append(str(item.get("id") or "").strip())
+        # Filter empty IDs and sort + dedupe case-insensitively so
+        # the /model picker renders a stable, alphabetical order.
+        # Built-in providers (anthropic/xai/github) already sort
+        # their catalogs; this custom OpenAI-compatible path did
+        # not, so the ordering changed with every /v1/models
+        # response and cache refresh.
+        models = [m for m in probed if m]
+        models = sorted(set(models), key=str.lower)
         return _probe_result(
-            probed, url, candidate_base.rstrip("/"),
+            models, url, candidate_base.rstrip("/"),
             alternate_base if alternate_base != candidate_base else normalized, is_fallback)
 
     if _neg_key is not None and all_timed_out: